b9928dd900c6c92486cfdab8fca797ac76a742ed
Suqatri <paul@suqatri.net>
2022-07-09 15:30:33 +0200

    Update ICloudServiceTemplate.java

--- api/api/src/main/java/net/suqatri/cloud/api/template/ICloudServiceTemplate.java
+++ api/api/src/main/java/net/suqatri/cloud/api/template/ICloudServiceTemplate.java
@@ -1,4 +1,11 @@
 package net.suqatri.cloud.api.template;
 
+import java.io.File;
+
 public interface ICloudServiceTemplate {
+
+    String getName();
+    File getTemplateFolder();
+    void updateToNodes();
+
 }

94ba26daf5c53b70ec8167d9494952c91e6fd78d
Suqatri <paul@suqatri.net>
2022-07-09 15:29:02 +0200

    Update MinecraftCloudAPI.java

--- api/api-minecraft/src/main/java/net/suqatri/cloud/api/minecraft/MinecraftCloudAPI.java
+++ api/api-minecraft/src/main/java/net/suqatri/cloud/api/minecraft/MinecraftCloudAPI.java
@@ -1,16 +1,72 @@
 package net.suqatri.cloud.api.minecraft;
 
 import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.console.ICommandManager;
 import net.suqatri.cloud.api.console.IConsole;
+import net.suqatri.cloud.api.event.ICloudEventManager;
+import net.suqatri.cloud.api.group.ICloudGroupManager;
+import net.suqatri.cloud.api.impl.CloudAPIImpl;
+import net.suqatri.cloud.api.node.ICloudNodeManager;
+import net.suqatri.cloud.api.packet.ICloudPacketManager;
+import net.suqatri.cloud.api.player.ICloudPlayerManager;
+import net.suqatri.cloud.api.service.ICloudServiceManager;
+import net.suqatri.cloud.api.service.factory.ICloudServiceFactory;
+import net.suqatri.cloud.api.template.ICloudServiceTemplateManager;
+import net.suqatri.cloud.api.utils.ApplicationType;
 
-public class MinecraftCloudAPI extends CloudAPI {
+public class MinecraftCloudAPI extends CloudAPIImpl {
 
     public MinecraftCloudAPI(){
-
+        super(ApplicationType.SERVICE_MINECRAFT);
     }
 
     @Override
     public IConsole getConsole() {
+        return null;
+    }
+
+    @Override
+    public ICommandManager<?> getCommandManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudGroupManager getGroupManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudNodeManager getNodeManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudPacketManager getPacketManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudPlayerManager getPlayerManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudServiceFactory getServiceFactory() {
+        return null;
+    }
+
+    @Override
+    public ICloudServiceManager getServiceManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudServiceTemplateManager getServiceTemplateManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudEventManager getEventManager() {
         return null;
     }
 }

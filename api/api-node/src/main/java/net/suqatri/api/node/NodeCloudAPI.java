8625e855b15e12d75e37b8119a63c3cdef85a903
Suqatri <paul@suqatri.net>
2022-07-09 15:29:05 +0200

    Update NodeCloudAPI.java

--- api/api-node/src/main/java/net/suqatri/api/node/NodeCloudAPI.java
+++ api/api-node/src/main/java/net/suqatri/api/node/NodeCloudAPI.java
@@ -1,6 +1,16 @@
 package net.suqatri.api.node;
 
 import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.impl.CloudAPIImpl;
+import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.utils.ApplicationType;
 
-public class NodeCloudAPI extends CloudAPI {
+public abstract class NodeCloudAPI extends CloudAPIImpl {
+
+    public NodeCloudAPI() {
+        super(ApplicationType.NODE);
+    }
+
+    protected abstract ICloudNode getCloudNode();
+
 }

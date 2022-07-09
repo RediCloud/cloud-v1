faa749d2540a22150d62d0af906900d46810ed1c
Suqatri <paul@suqatri.net>
2022-07-09 15:34:06 +0200

    Create Answers.java

---
+++ node/node-console/src/main/java/net/suqatri/cloud/node/console/setup/annotations/Answers.java
@@ -0,0 +1,13 @@
+package net.suqatri.cloud.node.console.setup.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(java.lang.annotation.ElementType.FIELD)
+public @interface Answers {
+
+    String[] forbidden() default {};
+    String[] only() default {};
+
+}

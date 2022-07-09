b97ce3c65e4de61b2280b29174460864d7c966fe
Suqatri <paul@suqatri.net>
2022-07-09 15:29:42 +0200

    Update ICloudServiceFactory.java

--- api/api/src/main/java/net/suqatri/cloud/api/service/factory/ICloudServiceFactory.java
+++ api/api/src/main/java/net/suqatri/cloud/api/service/factory/ICloudServiceFactory.java
@@ -1,4 +1,10 @@
 package net.suqatri.cloud.api.service.factory;
 
+import net.suqatri.cloud.api.service.ICloudService;
+import net.suqatri.cloud.api.service.IServiceStartConfiguration;
+
 public interface ICloudServiceFactory {
+
+    ICloudService createCloudService(IServiceStartConfiguration configuration);
+
 }

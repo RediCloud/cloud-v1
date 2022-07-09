6db41a28a1359de3db29a23338eb3302294c7b90
Suqatri <paul@suqatri.net>
2022-07-09 15:30:26 +0200

    Update ICloudService.java

--- api/api/src/main/java/net/suqatri/cloud/api/service/ICloudService.java
+++ api/api/src/main/java/net/suqatri/cloud/api/service/ICloudService.java
@@ -1,4 +1,37 @@
 package net.suqatri.cloud.api.service;
 
+import net.suqatri.cloud.api.group.ICloudGroup;
+
+import java.util.UUID;
+
 public interface ICloudService {
+
+    IServiceStartConfiguration getConfiguration();
+
+    default ServiceEnvironment getEnvironment(){
+        return getConfiguration().getEnvironment();
+    }
+
+    default String getName(){
+        return getConfiguration().getName();
+    }
+    default UUID getUniqueId() { return getConfiguration().getUniqueId(); }
+    default int getId(){
+        return getConfiguration().getId();
+    }
+
+    ICloudGroup getGroup();
+
+    String getMotd();
+    void setMotd(String motd);
+
+    int getMaxPlayers();
+    void setMaxPlayers(int maxPlayers);
+
+    default boolean isStatic(){
+        return getConfiguration().isStatic();
+    }
+
+
+
 }

c1c96c80ea859000319e9553213f5e5d4ae7bf80
Suqatri <paul@suqatri.net>
2022-07-09 15:25:38 +0200

    Impl Event Invoker

---
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/event/CloudEventInvoker.java
@@ -0,0 +1,26 @@
+package net.suqatri.cloud.api.impl.event;
+
+import net.suqatri.cloud.api.event.ICloudEventInvoker;
+
+import java.lang.reflect.Method;
+
+public class CloudEventInvoker implements ICloudEventInvoker {
+
+    private final Object listener;
+    private final Method method;
+
+    public CloudEventInvoker(Object listener, Method method) {
+        this.listener = listener;
+        this.method = method;
+    }
+
+    @Override
+    public Object getListener() {
+        return this.listener;
+    }
+
+    @Override
+    public Method getMethod() {
+        return this.method;
+    }
+}

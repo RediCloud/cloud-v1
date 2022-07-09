93c2f0b82ce3103ca7f8a3bc5c550bc9e2546ec7
Suqatri <paul@suqatri.net>
2022-07-09 15:32:07 +0200

    Create ClassLoaderDebug.java

---
+++ node/node-runner/src/main/java/net/suqatri/cloud/runner/dependency/classloader/ClassLoaderDebug.java
@@ -0,0 +1,14 @@
+package net.suqatri.cloud.runner.dependency.classloader;
+
+public class ClassLoaderDebug {
+
+    public ClassLoaderDebug(){
+        System.out.println("System Class Loader:");
+        System.out.println("- " + ClassLoader.getSystemClassLoader().getClass().getName());
+        System.out.println("Current Class Loader:");
+        System.out.println("- " + this.getClass().getClassLoader().getClass().getName());
+        System.out.println("Thread Context Class Loader:");
+        System.out.println("- " + Thread.currentThread().getContextClassLoader().getClass().getName());
+    }
+
+}

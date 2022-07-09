96a25f9d16154f687f0ec521fbae66dd5873cf9e
Suqatri <paul@suqatri.net>
2022-07-09 15:29:34 +0200

    Update ICloudOfflinePlayer.java

--- api/api/src/main/java/net/suqatri/cloud/api/player/ICloudOfflinePlayer.java
+++ api/api/src/main/java/net/suqatri/cloud/api/player/ICloudOfflinePlayer.java
@@ -1,4 +1,14 @@
 package net.suqatri.cloud.api.player;
 
+import java.util.UUID;
+
 public interface ICloudOfflinePlayer {
+
+    UUID getUniqueId();
+    String getName();
+    long getFirstLogin();
+    long getLastLogin();
+    long getLastLogout();
+    String getLastIp();
+
 }

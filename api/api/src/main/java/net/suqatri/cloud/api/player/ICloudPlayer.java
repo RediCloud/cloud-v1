215e0b997bdbda4df462dc3658fa817fc4ac4f19
Suqatri <paul@suqatri.net>
2022-07-09 15:29:36 +0200

    Update ICloudPlayer.java

--- api/api/src/main/java/net/suqatri/cloud/api/player/ICloudPlayer.java
+++ api/api/src/main/java/net/suqatri/cloud/api/player/ICloudPlayer.java
@@ -1,4 +1,23 @@
 package net.suqatri.cloud.api.player;
 
-public interface ICloudPlayer {
+import net.suqatri.cloud.api.service.ICloudService;
+
+public interface ICloudPlayer extends ICloudOfflinePlayer{
+
+    ICloudService getServer();
+    ICloudService getProxy();
+
+    default long getSessionTime(){
+        return System.currentTimeMillis() - getLastLogin();
+    }
+
+    void sendMessage(String message);
+    void sendTitle(String title, String subTitle, int fadeIn, int stay, int fadeOut);
+    void sendActionbar(String message);
+    void sendTab(String header, String footer);
+
+    void connect(String server);
+    void connect(ICloudService cloudService);
+    void disconnect(String reason);
+
 }

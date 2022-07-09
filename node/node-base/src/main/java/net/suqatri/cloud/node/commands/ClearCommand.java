81fad1955dbb299b0570c5ca0039298acbfa16a7
Suqatri <paul@suqatri.net>
2022-07-09 15:31:00 +0200

    Create ClearCommand.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/commands/ClearCommand.java
@@ -0,0 +1,16 @@
+package net.suqatri.cloud.node.commands;
+
+import net.suqatri.cloud.node.NodeLauncher;
+import net.suqatri.commands.ConsoleCommand;
+import net.suqatri.commands.annotation.CommandAlias;
+import net.suqatri.commands.annotation.Default;
+
+@CommandAlias("clear")
+public class ClearCommand extends ConsoleCommand {
+
+    @Default
+    public void onClear(){
+        NodeLauncher.getInstance().getConsole().clearScreen();
+    }
+
+}

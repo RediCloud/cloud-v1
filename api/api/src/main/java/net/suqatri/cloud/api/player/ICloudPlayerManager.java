8dcdca1956316402a529e382f309a348d88cc504
Suqatri <paul@suqatri.net>
2022-07-09 15:29:38 +0200

    Update ICloudPlayerManager.java

--- api/api/src/main/java/net/suqatri/cloud/api/player/ICloudPlayerManager.java
+++ api/api/src/main/java/net/suqatri/cloud/api/player/ICloudPlayerManager.java
@@ -1,4 +1,19 @@
 package net.suqatri.cloud.api.player;
 
+import java.util.UUID;
+
 public interface ICloudPlayerManager {
+
+    ICloudPlayer getPlayer(String playerName);
+    ICloudPlayer getPlayer(UUID uniqueId);
+
+    ICloudOfflinePlayer getOfflinePlayer(String playerName);
+    ICloudOfflinePlayer getOfflinePlayer(UUID uniqueId);
+
+    boolean isConnected(String playerName);
+    boolean isConnected(UUID uniqueId);
+
+    int getRegisteredCount();
+    int getOnlineCount();
+
 }

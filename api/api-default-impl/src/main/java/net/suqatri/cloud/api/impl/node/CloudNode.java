60412bc48215006d7e4683f57499d6169031fa3d
Suqatri <paul@suqatri.net>
2022-07-11 01:24:37 +0200

    Add CloudNode#shutdown

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/node/CloudNode.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/node/CloudNode.java
@@ -3,7 +3,10 @@ package net.suqatri.cloud.api.impl.node;
 import lombok.Getter;
 import lombok.Setter;
 import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.impl.node.packet.CloudNodeShutdownPacket;
 import net.suqatri.cloud.api.impl.redis.bucket.RBucketObject;
+import net.suqatri.cloud.api.network.INetworkComponentInfo;
+import net.suqatri.cloud.api.network.NetworkComponentType;
 import net.suqatri.cloud.api.node.ICloudNode;
 import net.suqatri.cloud.api.service.ICloudService;
 import net.suqatri.cloud.commons.function.future.FutureAction;
@@ -28,6 +31,29 @@ public class CloudNode extends RBucketObject implements ICloudNode {
     private int maxMemory;
     private int maxParallelStartingServiceCount;
     private int maxServiceCount;
+    private long lastConnection = 0L;
+    private long lastStart = 0L;
+
+    public long getUpTime(){
+        return System.currentTimeMillis() - this.lastStart;
+    }
+
+    @Override
+    public void shutdown() {
+        if(CloudAPI.getInstance().getNetworkComponentInfo().equals(this.getNetworkComponentInfo())){
+            CloudAPI.getInstance().shutdown(false);
+            return;
+        }
+        CloudNodeShutdownPacket packet = new CloudNodeShutdownPacket();
+        packet.setNodeId(this.uniqueId);
+        packet.getPacketData().addReceiver(getNetworkComponentInfo());
+        packet.publishAsync();
+    }
+
+    @Override
+    public INetworkComponentInfo getNetworkComponentInfo() {
+        return CloudAPI.getInstance().getNetworkComponentManager().getComponentInfo(this);
+    }
 
     @Override
     public FutureAction<Collection<ICloudService>> getStartedServices() {

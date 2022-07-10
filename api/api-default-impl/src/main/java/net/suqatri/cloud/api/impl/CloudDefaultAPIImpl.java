aa38aeb3851c143a2e8d4df3efb279fbdca1a422
Suqatri <paul@suqatri.net>
2022-07-11 01:23:14 +0200

    Add initShutdownHook

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/CloudDefaultAPIImpl.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/CloudDefaultAPIImpl.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 import net.suqatri.cloud.api.CloudAPI;
 import net.suqatri.cloud.api.event.ICloudEventManager;
 import net.suqatri.cloud.api.impl.event.CloudEventManager;
+import net.suqatri.cloud.api.impl.network.NetworkComponentManager;
 import net.suqatri.cloud.api.impl.packet.CloudPacketManager;
+import net.suqatri.cloud.api.network.INetworkComponentInfo;
+import net.suqatri.cloud.api.network.INetworkComponentManager;
 import net.suqatri.cloud.api.packet.ICloudPacketManager;
 import net.suqatri.cloud.api.redis.IRedisConnection;
 import net.suqatri.cloud.api.utils.ApplicationType;
@@ -15,14 +18,27 @@ public abstract class CloudDefaultAPIImpl extends CloudAPI {
     @Getter
     private static CloudDefaultAPIImpl instance;
 
-    private ICloudEventManager eventManager;
-    private ICloudPacketManager packetManager;
+    private final ICloudEventManager eventManager;
+    private final ICloudPacketManager packetManager;
+    private final INetworkComponentManager networkComponentManager;
 
     public CloudDefaultAPIImpl(ApplicationType type) {
         super(type);
         instance = this;
-        this.packetManager = new CloudPacketManager();
         this.eventManager = new CloudEventManager();
+        this.packetManager = new CloudPacketManager();
+        this.networkComponentManager = new NetworkComponentManager();
+    }
+
+    @Override
+    public void initShutdownHook(){
+        if(this.isShutdownHookAdded()) return;
+        Runtime.getRuntime().addShutdownHook(new Thread("node-shutdown-hook"){
+            @Override
+            public void run(){
+                CloudAPI.getInstance().shutdown(true);
+            }
+        });
     }
 
     public abstract IRedisConnection getRedisConnection();

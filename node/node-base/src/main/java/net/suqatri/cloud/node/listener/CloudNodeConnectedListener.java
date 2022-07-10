3609ec1c46c754717c52378e50952901afcf7f8c
Suqatri <paul@suqatri.net>
2022-07-11 01:30:06 +0200

    Create CloudNodeConnectedListener.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/listener/CloudNodeConnectedListener.java
@@ -0,0 +1,25 @@
+package net.suqatri.cloud.node.listener;
+
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.event.CloudListener;
+import net.suqatri.cloud.api.node.event.CloudNodeConnectedEvent;
+import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.node.NodeLauncher;
+
+public class CloudNodeConnectedListener {
+
+    @CloudListener
+    public void onCloudNodeConnected(CloudNodeConnectedEvent event) {
+        event.getCloudNodeAsync()
+            .whenComplete((nodeHolder, t) -> {
+               if(t != null){
+                   CloudAPI.getInstance().getConsole().error("Error while getting new connected node information!", t);
+                   return;
+               }
+               if(nodeHolder.get().getUniqueId().equals(NodeLauncher.getInstance().getCloudNode().get().getUniqueId())) return;
+               CloudAPI.getInstance().getConsole().info("Node " + NodeLauncher.getInstance().getConsole().getHighlightColor() + nodeHolder.get().getName() + NodeLauncher.getInstance().getConsole().getTextColor() + " has been connected to the cluster!");
+            });
+    }
+
+}

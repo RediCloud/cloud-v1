b2f1f0b036b5a36556f03eaaafd22e632bfa543e
Suqatri <paul@suqatri.net>
2022-07-11 01:29:44 +0200

    Update IRBucketHolder.java

--- api/api/src/main/java/net/suqatri/cloud/api/redis/bucket/IRBucketHolder.java
+++ api/api/src/main/java/net/suqatri/cloud/api/redis/bucket/IRBucketHolder.java
@@ -4,8 +4,10 @@ import net.suqatri.cloud.commons.function.future.FutureAction;
 
 public interface IRBucketHolder<T> {
 
-    T getObject(boolean force);
-    FutureAction<T> getObjectAsync(boolean force);
+    default T get() {
+        return this.get(false);
+    }
+    T get(boolean force);
 
     IRBucketHolder<T> update(T object);
     FutureAction<IRBucketHolder<T>> updateAsync(T object);

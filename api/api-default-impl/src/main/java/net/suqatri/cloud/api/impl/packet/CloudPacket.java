9f4d74b5ab91d91a73f20618cd333d962e75267d
Suqatri <paul@suqatri.net>
2022-07-11 01:26:08 +0200

    Add option to publish a packet to all connected network components

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/packet/CloudPacket.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/packet/CloudPacket.java
@@ -5,13 +5,36 @@ import net.suqatri.cloud.api.packet.ICloudPacket;
 
 public abstract class CloudPacket implements ICloudPacket {
 
+    private final CloudPacketData packetData = new CloudPacketData();
+
+    @Override
+    public CloudPacketData getPacketData() {
+        return this.packetData;
+    }
+
     @Override
     public void publish() {
+        if(getPacketData().getReceivers().isEmpty()) throw new IllegalStateException("No receivers specified!");
+        getPacketData().setSender(CloudAPI.getInstance().getNetworkComponentInfo());
         CloudAPI.getInstance().getPacketManager().publish(this);
     }
 
     @Override
     public void publishAsync() {
+        if(getPacketData().getReceivers().isEmpty()) throw new IllegalStateException("No receivers specified!");
+        getPacketData().setSender(CloudAPI.getInstance().getNetworkComponentInfo());
+        CloudAPI.getInstance().getPacketManager().publishAsync(this);
+    }
+
+    @Override
+    public void publishAll() {
+        getPacketData().setSender(CloudAPI.getInstance().getNetworkComponentInfo());
+        CloudAPI.getInstance().getPacketManager().publish(this);
+    }
+
+    @Override
+    public void publishAllAsync() {
+        getPacketData().setSender(CloudAPI.getInstance().getNetworkComponentInfo());
         CloudAPI.getInstance().getPacketManager().publishAsync(this);
     }
 }

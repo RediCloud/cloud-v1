6cc3d3c82439d9dcc165cee74fcdded3fe979322
Suqatri <paul@suqatri.net>
2022-07-11 01:30:12 +0200

    Update ClusterConnectionInformation.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/node/ClusterConnectionInformation.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/node/ClusterConnectionInformation.java
@@ -11,20 +11,10 @@ import java.util.UUID;
 @Setter
 public class ClusterConnectionInformation implements Serializable {
 
-    private String name;
     private UUID uniqueId;
-    private String hostName;
-    private int maxMemory;
-    private int maxServiceCount;
-    private int maxParallelServiceCount;
 
     public void applyToNode(CloudNode cloudNode){
-        cloudNode.setName(this.name);
         cloudNode.setUniqueId(this.uniqueId);
-        cloudNode.setHostname(this.hostName);
-        cloudNode.setMaxMemory(this.maxMemory);
-        cloudNode.setMaxServiceCount(this.maxServiceCount);
-        cloudNode.setMaxParallelStartingServiceCount(this.maxParallelServiceCount);
     }
 
 }

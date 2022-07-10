bfc8c80204ef28e4b9aecf9cb417b9a6dc9b9ed8
Suqatri <paul@suqatri.net>
2022-07-11 01:29:51 +0200

    Update ApplicationType.java

--- api/api/src/main/java/net/suqatri/cloud/api/utils/ApplicationType.java
+++ api/api/src/main/java/net/suqatri/cloud/api/utils/ApplicationType.java
@@ -1,8 +1,17 @@
 package net.suqatri.cloud.api.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.suqatri.cloud.api.network.NetworkComponentType;
+
+@AllArgsConstructor
+@Getter
 public enum ApplicationType {
-    NODE,
-    SERVICE_MINECRAFT,
-    SERVICE_PROXY
+
+    NODE(NetworkComponentType.NODE),
+    SERVICE_MINECRAFT(NetworkComponentType.SERVICE),
+    SERVICE_PROXY(NetworkComponentType.SERVICE);
+
+    private final NetworkComponentType networkComponentType;
 
 }

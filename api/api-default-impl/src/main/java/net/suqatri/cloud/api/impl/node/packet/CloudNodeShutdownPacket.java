44acce64160197efbb9c63d74c7e4991836a9ce8
Suqatri <paul@suqatri.net>
2022-07-11 01:24:59 +0200

    Create CloudNodeShutdownPacket for remote shutdown request

---
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/node/packet/CloudNodeShutdownPacket.java
@@ -0,0 +1,26 @@
+package net.suqatri.cloud.api.impl.node.packet;
+
+import lombok.Data;
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.impl.node.CloudNode;
+import net.suqatri.cloud.api.network.NetworkComponentType;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+@Data
+public class CloudNodeShutdownPacket extends CloudNodePacket {
+
+    private UUID nodeId;
+
+    @Override
+    public void receive() {
+        if(CloudAPI.getInstance().getNetworkComponentInfo().getType() != NetworkComponentType.NODE) {
+            CloudAPI.getInstance().getConsole().error("The " + this.getClass().getSimpleName() + " can only be received by the node!");
+            return;
+        }
+        CloudAPI.getInstance().getNodeManager().getNodeAsync(this.nodeId)
+                .onFailure(e -> CloudAPI.getInstance().getConsole().error("Failed to get node " + this.nodeId, (Throwable) e))
+                .onSuccess((Consumer<CloudNode>) CloudNode::shutdown);
+    }
+}

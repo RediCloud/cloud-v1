0359eeb6ae5b3c8322540895a578e5ca4f04dd42
Suqatri <paul@suqatri.net>
2022-07-11 14:45:43 +0200

    Update ICloudGroupManager.java

--- api/api/src/main/java/net/suqatri/cloud/api/group/ICloudGroupManager.java
+++ api/api/src/main/java/net/suqatri/cloud/api/group/ICloudGroupManager.java
@@ -1,4 +1,27 @@
 package net.suqatri.cloud.api.group;
 
-public interface ICloudGroupManager {
+import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.commons.function.future.FutureAction;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public interface ICloudGroupManager<T extends ICloudGroup> {
+
+    FutureAction<IRBucketHolder<T>> getGroupAsync(UUID uniqueId);
+    IRBucketHolder<T> getGroup(UUID uniqueId);
+
+    FutureAction<IRBucketHolder<T>> getGroupAsync(String name);
+    IRBucketHolder<T> getGroup(String name);
+
+    FutureAction<Boolean> existsGroupAsync(UUID uniqueId);
+    boolean existsGroup(UUID uniqueId);
+
+    FutureAction<Boolean> existsGroupAsync(String name);
+    boolean existsGroup(String name);
+
+    Collection<IRBucketHolder<T>> getGroups();
+    FutureAction<Collection<IRBucketHolder<T>>> getGroupsAsync();
+
 }

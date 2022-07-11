3b6576332af93f6351e3793424cd8b608962d7e4
Suqatri <paul@suqatri.net>
2022-07-11 18:09:27 +0200

    Update ICloudServiceTemplateManager.java

--- api/api/src/main/java/net/suqatri/cloud/api/template/ICloudServiceTemplateManager.java
+++ api/api/src/main/java/net/suqatri/cloud/api/template/ICloudServiceTemplateManager.java
@@ -1,11 +1,22 @@
 package net.suqatri.cloud.api.template;
 
-import java.io.File;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.commons.function.future.FutureAction;
+
+import java.util.Collection;
+
 
 public interface ICloudServiceTemplateManager {
 
-    ICloudServiceTemplate getTemplate(String name);
-    boolean existsTemplate(String name);
-    void createTemplate(String name, File folder);
+    IRBucketHolder<ICloudServiceTemplate> getTemplate(String name);
+    FutureAction<IRBucketHolder<ICloudServiceTemplate>> getTemplateAsync(String name);
 
+    boolean existsTemplate(String name);
+    FutureAction<Boolean> existsTemplateAsync(String name);
+
+    IRBucketHolder<ICloudServiceTemplate> createTemplate(String name);
+    FutureAction<IRBucketHolder<ICloudServiceTemplate>> createTemplateAsync(String name);
+
+    Collection<IRBucketHolder<ICloudServiceTemplate>> getAllTemplates();
+    FutureAction<Collection<IRBucketHolder<ICloudServiceTemplate>>> getAllTemplatesAsync();
 }

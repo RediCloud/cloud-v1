629af17ef824b79f497d536c3cbd8f6cd7766278
Suqatri <paul@suqatri.net>
2022-07-12 01:01:08 +0200

    Create FileTransferCompletedPacket.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/packet/FileTransferCompletedPacket.java
@@ -0,0 +1,11 @@
+package net.suqatri.cloud.node.file.packet;
+
+import net.suqatri.cloud.api.impl.packet.CloudPacket;
+
+public class FileTransferCompletedPacket extends FileTransferPacket {
+
+    @Override
+    public void receive() {
+
+    }
+}

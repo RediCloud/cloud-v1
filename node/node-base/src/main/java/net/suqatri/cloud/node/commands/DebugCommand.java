dc6709bd95f2797e56a797f6de82e23bb89a7712
Suqatri <paul@suqatri.net>
2022-07-11 13:30:32 +0200

    Update DebugCommand.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/commands/DebugCommand.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/commands/DebugCommand.java
@@ -8,23 +8,9 @@ import net.suqatri.commands.ConsoleCommand;
 import net.suqatri.commands.annotation.CommandAlias;
 import net.suqatri.commands.annotation.Subcommand;
 
+import java.lang.reflect.Method;
+
 @CommandAlias("debug")
 public class DebugCommand extends ConsoleCommand {
-
-    @Subcommand("event listeners")
-    public void onEventListener(CommandSender commandSender){
-        commandSender.sendMessage("");
-        commandSender.sendMessage("Event listeners:");
-        ((CloudEventManager)CloudAPI.getInstance().getEventManager()).getByEventBaked().forEach((eventClass, invokers) -> {
-            commandSender.sendMessage(" - " + eventClass.getSimpleName());
-            for (CloudEventInvoker invoker : invokers) {
-                commandSender.sendMessage("     -> Class" + invoker.getListener().getClass().getName());
-                commandSender.sendMessage("     -> Methode" + invoker.getMethod().getName());
-                commandSender.sendMessage("-----");
-            }
-            commandSender.sendMessage("_-----------------_");
-        });
-        commandSender.sendMessage("");
-    }
 
 }

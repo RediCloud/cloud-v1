8d179566b7268df5c438ec6f3d873cfcf1bc3e3f
Suqatri <paul@suqatri.net>
2022-07-11 14:45:19 +0200

    Create CloudGroup.java

---
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/group/CloudGroup.java
@@ -0,0 +1,154 @@
+package net.suqatri.cloud.api.impl.group;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.group.ICloudGroup;
+import net.suqatri.cloud.api.impl.redis.bucket.RBucketObject;
+import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.player.ICloudPlayer;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.api.service.ICloudService;
+import net.suqatri.cloud.api.service.IServiceStartConfiguration;
+import net.suqatri.cloud.api.service.ServiceState;
+import net.suqatri.cloud.api.template.ICloudServiceTemplate;
+import net.suqatri.cloud.commons.function.future.FutureAction;
+import net.suqatri.cloud.commons.function.future.FutureActionCollection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+public class CloudGroup extends RBucketObject implements ICloudGroup {
+
+    private UUID uniqueId;
+    private String name;
+    @Setter
+    private String[] processParameters;
+    @Setter
+    private String[] jvmArguments;
+    @Setter
+    private String javaCommand;
+    @Setter
+    private int minServices;
+    @Setter
+    private int maxServices;
+    private Collection<String> templatesNames;
+    @Setter
+    private boolean staticGroup;
+    private Collection<UUID> associatedNodesIds;
+    @Setter
+    private int startPort;
+    @Setter
+    private boolean maintenance;
+    @Setter
+    private int maxMemory;
+    @Setter
+    private int startPriority;
+
+    @Override
+    public FutureAction<Collection<IRBucketHolder<ICloudServiceTemplate>>> getTemplates() {
+        FutureAction<Collection<IRBucketHolder<ICloudServiceTemplate>>> futureAction = new FutureAction<>();
+
+        //TODO SErviceTEmplateManager
+
+        return futureAction;
+    }
+
+    @Override
+    public void setTemplates(Collection<ICloudServiceTemplate> templates) {
+        this.templatesNames = templates.stream().map(ICloudServiceTemplate::getName).collect(Collectors.toList());
+    }
+
+    @Override
+    public void addTemplate(ICloudServiceTemplate template) {
+        this.templatesNames.add(template.getName());
+    }
+
+    @Override
+    public void removeTemplate(ICloudServiceTemplate template) {
+        this.templatesNames.remove(template.getName());
+    }
+
+    @Override
+    public boolean hasTemplate(ICloudServiceTemplate template) {
+        return this.templatesNames.contains(template.getName());
+    }
+
+    @Override
+    public int getOnlineServiceCount() {
+        return 0; //TODO ServiceManager
+    }
+
+    @Override
+    public int getServicesCount(ServiceState serviceState) {
+        return 0; //TODO ServiceManager
+    }
+
+    @Override
+    public Collection<ICloudService> getOnlineServices() {
+        return null; //TODO ServiceManager
+    }
+
+    @Override
+    public Collection<ICloudService> getOnlineServices(ServiceState serviceState) {
+        return null; //TODO ServiceManager
+    }
+
+    @Override
+    public boolean isStatic() {
+        return this.staticGroup;
+    }
+
+    @Override
+    public void setStatic(boolean staticGroup) {
+        this.staticGroup = staticGroup;
+    }
+
+    @Override
+    public FutureAction<Collection<IRBucketHolder<ICloudNode>>> getAssociatedNodes() {
+        FutureActionCollection<UUID, IRBucketHolder<ICloudNode>> futureActionCollection = new FutureActionCollection<>();
+        for (UUID nodeId : this.associatedNodesIds) {
+            futureActionCollection.addToProcess(nodeId, CloudAPI.getInstance().getNodeManager().getNodeAsync(nodeId));
+        }
+        return futureActionCollection.process().map(HashMap::values);
+    }
+
+    @Override
+    public void addAssociatedNode(ICloudNode node) {
+        this.associatedNodesIds.remove(node.getUniqueId());
+    }
+
+    @Override
+    public void removeAssociatedNode(ICloudNode node) {
+        this.associatedNodesIds.remove(node.getUniqueId());
+    }
+
+    @Override
+    public boolean hasAssociatedNode(ICloudNode node) {
+        return this.associatedNodesIds.contains(node.getUniqueId());
+    }
+
+    @Override
+    public void setAssociatedNodes(Collection<ICloudNode> nodes) {
+        this.associatedNodesIds.clear();
+        this.associatedNodesIds.addAll(nodes.stream().map(ICloudNode::getUniqueId).collect(Collectors.toList()));
+    }
+
+    @Override
+    public Collection<ICloudPlayer> getPlayers() {
+        return null; //TODO PlayerManager / serviceManager
+    }
+
+    @Override
+    public int getPlayersCount() {
+        return 0; //TODO playerManager / serviceManager
+    }
+
+    @Override
+    public IServiceStartConfiguration createServiceConfiguration() {
+        return null; // TODO ServiceManager
+    }
+}

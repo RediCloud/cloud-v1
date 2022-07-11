e9ac335840cbd15330e0163b97a38eaf0e803f20
Suqatri <paul@suqatri.net>
2022-07-11 18:08:48 +0200

    Create CloudServiceTemplateManager.java

---
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/template/CloudServiceTemplateManager.java
@@ -0,0 +1,63 @@
+package net.suqatri.cloud.api.impl.template;
+
+import net.suqatri.cloud.api.impl.redis.bucket.RedissonBucketManager;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.api.template.ICloudServiceTemplate;
+import net.suqatri.cloud.api.template.ICloudServiceTemplateManager;
+import net.suqatri.cloud.commons.function.future.FutureAction;
+
+import java.io.File;
+import java.util.Collection;
+
+public class CloudServiceTemplateManager extends RedissonBucketManager<CloudServiceTemplate, ICloudServiceTemplate> implements ICloudServiceTemplateManager {
+
+    public CloudServiceTemplateManager() {
+        super("template", ICloudServiceTemplate.class);
+    }
+
+    @Override
+    public IRBucketHolder<ICloudServiceTemplate> getTemplate(String name) {
+        return this.getBucketHolder(name);
+    }
+
+    @Override
+    public FutureAction<IRBucketHolder<ICloudServiceTemplate>> getTemplateAsync(String name) {
+        return this.getBucketHolderAsync(name);
+    }
+
+    @Override
+    public boolean existsTemplate(String name) {
+        return this.existsBucket(name);
+    }
+
+    @Override
+    public FutureAction<Boolean> existsTemplateAsync(String name) {
+        return this.existsBucketAsync(name);
+    }
+
+    @Override
+    public IRBucketHolder<ICloudServiceTemplate> createTemplate(String name) {
+        CloudServiceTemplate template = new CloudServiceTemplate();
+        template.setName(name);
+        //TODO FileManager
+        return this.createBucket(name, template);
+    }
+
+    @Override
+    public FutureAction<IRBucketHolder<ICloudServiceTemplate>> createTemplateAsync(String name) {
+        CloudServiceTemplate template = new CloudServiceTemplate();
+        template.setName(name);
+        //TODO FileManager
+        return this.createBucketAsync(name, template);
+    }
+
+    @Override
+    public Collection<IRBucketHolder<ICloudServiceTemplate>> getAllTemplates() {
+        return this.getBucketHolders();
+    }
+
+    @Override
+    public FutureAction<Collection<IRBucketHolder<ICloudServiceTemplate>>> getAllTemplatesAsync() {
+        return this.getBucketHoldersAsync();
+    }
+}

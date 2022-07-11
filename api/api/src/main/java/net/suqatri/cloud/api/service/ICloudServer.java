eba679a4f5ea2727aced7e70a60be12eb5d959aa
Suqatri <paul@suqatri.net>
2022-07-11 18:06:41 +0200

    Delete ICloudService Impl

--- api/api/src/main/java/net/suqatri/cloud/api/service/ICloudServer.java
+++
@@ -1,9 +0,0 @@
-package net.suqatri.cloud.api.service;
-
-public interface ICloudServer extends ICloudService{
-
-    @Override
-    default ServiceEnvironment getEnvironment() {
-        return ServiceEnvironment.MINECRAFT;
-    }
-}

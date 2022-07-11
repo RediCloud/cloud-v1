0c281d2b81f11c553de540719d6347718b7a3e71
Suqatri <paul@suqatri.net>
2022-07-11 13:30:23 +0200

    Update LogLevel.java

--- api/api/src/main/java/net/suqatri/cloud/api/console/LogLevel.java
+++ api/api/src/main/java/net/suqatri/cloud/api/console/LogLevel.java
@@ -3,15 +3,18 @@ package net.suqatri.cloud.api.console;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.awt.*;
+
 @Getter
 @AllArgsConstructor
 public enum LogLevel {
 
-    DEBUG(1),
-    INFO(2),
-    WARN(3),
-    ERROR(4),
-    FATAL(5);
+    DEBUG(1, Color.ORANGE),
+    INFO(2, Color.CYAN),
+    WARN(3, Color.RED.brighter()),
+    ERROR(4, Color.RED),
+    FATAL(5, Color.RED.darker());
 
     private int id;
+    private Color color;
 }

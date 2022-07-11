664709962ea2ed15489d811537d5f88d09bc1129
Suqatri <paul@suqatri.net>
2022-07-11 13:30:26 +0200

    Update CloudListener.java

--- api/api/src/main/java/net/suqatri/cloud/api/event/CloudListener.java
+++ api/api/src/main/java/net/suqatri/cloud/api/event/CloudListener.java
@@ -1,8 +1,11 @@
 package net.suqatri.cloud.api.event;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CloudListener {
     byte priority() default EventPriority.NORMAL;

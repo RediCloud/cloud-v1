ddfc0cca105715dbb4e00a75de621e8095e2a519
Suqatri <paul@suqatri.net>
2022-07-12 01:00:01 +0200

    Update MemorySuggester.java

--- node/node-console/src/main/java/net/suqatri/cloud/node/console/setup/suggester/MemorySuggester.java
+++ node/node-console/src/main/java/net/suqatri/cloud/node/console/setup/suggester/MemorySuggester.java
@@ -4,13 +4,18 @@ import net.suqatri.cloud.node.console.setup.Setup;
 import net.suqatri.cloud.node.console.setup.SetupEntry;
 import net.suqatri.cloud.node.console.setup.SetupSuggester;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemorySuggester implements SetupSuggester {
 
     @Override
     public List<String> suggest(Setup<?> setup, SetupEntry entry) {
-        return Arrays.asList("512", "1024", "2048", "4096", "8192", "16384", "32768", "65536");
+        List<Integer> list = Arrays.asList(512, 1024, 2048, 4096, 8192, 16384, 32768, 65536);
+        Collections.reverse(list);
+        return list.parallelStream().map(String::valueOf).collect(Collectors.toList());
     }
 }

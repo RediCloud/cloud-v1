86d35b651ea5b10a3a6526e132f9a1ea97c71cdf
Suqatri <paul@suqatri.net>
2022-07-11 18:09:21 +0200

    Create ICloudServiceVersion.java

---
+++ api/api/src/main/java/net/suqatri/cloud/api/service/version/ICloudServiceVersion.java
@@ -0,0 +1,34 @@
+package net.suqatri.cloud.api.service.version;
+
+import net.suqatri.cloud.api.service.ServiceEnvironment;
+import net.suqatri.cloud.commons.file.Files;
+
+import java.io.File;
+import java.io.Serializable;
+
+public interface ICloudServiceVersion extends Serializable {
+
+    String getName();
+    String getDownloadUrl();
+    void setDownloadUrl(String url);
+    ServiceEnvironment getEnvironmentType();
+    void setEnvironmentType(ServiceEnvironment environmentType);
+    boolean isPaperClip();
+    void setPaperClip(boolean isPaperClip);
+    default File getFile() {
+        return new File(Files.VERSIONS_FOLDER.getFile(), getName() + ".jar");
+    }
+    default File getPatchedFile(){
+        return new File(Files.VERSIONS_FOLDER.getFile(), getName() + ".jar.patched");
+    }
+    default boolean isPatched(){
+        return this.getPatchedFile().exists();
+    }
+    default boolean isDownloaded(){
+        return this.getFile().exists();
+    }
+    default boolean needPatch(){
+        return this.isPaperClip() && !this.isPatched();
+    }
+
+}

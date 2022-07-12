b23f7894546b8a88d1e9164b95c01d813b06d102
Suqatri <paul@suqatri.net>
2022-07-12 02:04:30 +0200

    Update FileTransferReceiveProcess.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/file/process/FileTransferReceiveProcess.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/process/FileTransferReceiveProcess.java
@@ -1,19 +1,44 @@
 package net.suqatri.cloud.node.file.process;
 
 import lombok.Data;
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.network.INetworkComponentInfo;
+import net.suqatri.cloud.commons.file.FileUtils;
+import net.suqatri.cloud.commons.file.Files;
+import net.suqatri.cloud.commons.file.ZipUtils;
 
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TreeMap;
 import java.util.UUID;
 
 @Data
-public class FileTransferReceiveProcess {
+public class FileTransferReceiveProcess implements IFileTransferProcess {
 
-    private UUID senderNodeId;
+    private INetworkComponentInfo senderNetworkComponentInfo;
     private final UUID transferId;
     private int indexes;
-    private HashMap<Integer, byte[]> receivedFileData;
+    private TreeMap<Integer, byte[]> receivedFileData = new TreeMap<>();
 
     private String originalFilePath;
     private String destinationFilePath;
 
+    @Override
+    public void process() {
+        try {
+            if(this.receivedFileData.size() != this.indexes){
+                CloudAPI.getInstance().getConsole().error("File-read-transfer data size is not equal to indexes");
+                return;
+            }
+            byte[] bytes = FileUtils.mergeByteArrays(new ArrayList<>(this.receivedFileData.values()));
+
+            File zipFile = new File(Files.TEMP_FOLDER.getFile(), this.transferId + ".zip");
+            FileUtils.bytesToFile(bytes, zipFile.getAbsolutePath());
+
+            ZipUtils.unzipDir(zipFile, this.originalFilePath);
+        } catch (IOException e) {
+            CloudAPI.getInstance().getConsole().error("File-read-transfer process error", e);
+        }
+    }
 }

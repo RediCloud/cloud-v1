2519c7dc9f8da105ed689b1967e81eb2c7a88e3c
Suqatri <paul@suqatri.net>
2022-07-12 02:05:22 +0200

    Update FileTransferBytesPacket.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/file/packet/FileTransferBytesPacket.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/packet/FileTransferBytesPacket.java
@@ -2,6 +2,9 @@ package net.suqatri.cloud.node.file.packet;
 
 import lombok.Data;
 import lombok.Setter;
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.node.NodeLauncher;
+import net.suqatri.cloud.node.file.process.FileTransferReceiveProcess;
 
 @Data
 @Setter
@@ -12,6 +15,11 @@ public class FileTransferBytesPacket extends FileTransferPacket {
 
     @Override
     public void receive() {
-
+        FileTransferReceiveProcess process = NodeLauncher.getInstance().getFileTransferManager().getWaitingReceiveProcesses().get(this.getTransferId());
+        if (process == null) {
+            CloudAPI.getInstance().getConsole().error("File-read-transfer process not found for transferId " + this.getTransferId() + "! Received bytes can´t be processed! ByteIndex#" + this.getIndexId());
+            return;
+        }
+        process.getReceivedFileData().put(this.getIndexId(), this.getFileData());
     }
 }

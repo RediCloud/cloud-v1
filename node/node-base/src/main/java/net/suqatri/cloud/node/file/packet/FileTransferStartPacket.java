37e9783c71033f9784a93c8846087a81fb527da0
Suqatri <paul@suqatri.net>
2022-07-12 02:05:28 +0200

    Update FileTransferStartPacket.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/file/packet/FileTransferStartPacket.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/packet/FileTransferStartPacket.java
@@ -2,16 +2,23 @@ package net.suqatri.cloud.node.file.packet;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.suqatri.cloud.api.impl.packet.CloudPacket;
+import net.suqatri.cloud.node.NodeLauncher;
+import net.suqatri.cloud.node.file.process.FileTransferReceiveProcess;
 
 @Setter @Getter
 public class FileTransferStartPacket extends FileTransferPacket {
 
     private String originalFilePath;
     private String destinationFilePath;
+    private int indexes;
 
     @Override
     public void receive() {
-
+        FileTransferReceiveProcess process = new FileTransferReceiveProcess(this.getTransferId());
+        process.setDestinationFilePath(this.destinationFilePath);
+        process.setOriginalFilePath(this.originalFilePath);
+        process.setIndexes(this.indexes);
+        process.setSenderNetworkComponentInfo(this.getPacketData().getSender());
+        NodeLauncher.getInstance().getFileTransferManager().getWaitingReceiveProcesses().put(this.getTransferId(), process);
     }
 }

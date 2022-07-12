4f2acf728198d28aff9d4e7c81d71e4ceb8e9589
Suqatri <paul@suqatri.net>
2022-07-12 02:05:19 +0200

    Update FileTransferProcessThread.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/file/FileTransferProcessThread.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/FileTransferProcessThread.java
@@ -2,41 +2,50 @@ package net.suqatri.cloud.node.file;
 
 import lombok.Getter;
 import net.suqatri.cloud.api.CloudAPI;
-import net.suqatri.cloud.api.network.NetworkComponentType;
-import net.suqatri.cloud.node.file.packet.FileTransferBytesPacket;
-import net.suqatri.cloud.node.file.packet.FileTransferCompletedPacket;
-import net.suqatri.cloud.node.file.packet.FileTransferStartPacket;
+import net.suqatri.cloud.node.file.process.FileTransferReceiveProcess;
 import net.suqatri.cloud.node.file.process.FileTransferSentProcess;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 public class FileTransferProcessThread extends Thread {
 
-    private final ConcurrentHashMap<UUID, FileTransferSentProcess> processes = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<UUID, FileTransferSentProcess> sentProcesses = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<UUID, FileTransferReceiveProcess> receiveProcesses = new ConcurrentHashMap<>();
 
-    private FileTransferSentProcess currentProcess;
-    private boolean sentStartPacket = false;
-    private int bytePacketsCount = 0;
-    private boolean completedPacket = false;
+    private FileTransferSentProcess currentSentProcess;
+    private FileTransferReceiveProcess currentReceiveProcess;
 
     @Override
     public void run() {
         try {
 
-            if(!this.processes.isEmpty()){
-                this.currentProcess = this.processes.remove(0);
-                CloudAPI.getInstance().getConsole().debug("Starting file transfer process for transfer id: " + this.currentProcess.getTransferId());
-                this.currentProcess.process();
+            if(!this.sentProcesses.isEmpty()){
+                this.currentSentProcess = this.sentProcesses.remove(0);
+                CloudAPI.getInstance().getConsole().debug("Starting file-sent-transfer process for transfer id: " + this.currentSentProcess.getTransferId());
+                this.currentSentProcess.process();
+                this.currentSentProcess = null;
+            }
+
+            Thread.sleep(500);
+
+            if(!this.receiveProcesses.isEmpty()){
+                this.currentReceiveProcess = this.receiveProcesses.remove(0);
+                CloudAPI.getInstance().getConsole().debug("Starting file-read-transfer process for transfer id: " + this.currentReceiveProcess.getTransferId());
+                this.currentReceiveProcess.process();
+                this.currentReceiveProcess = null;
             }
 
             Thread.sleep(500);
         } catch (Exception e) {
-            if(this.currentProcess != null){
-                this.currentProcess.getFutureAction().completeExceptionally(e);
-                this.currentProcess = null;
+            if(this.currentSentProcess != null){
+                this.currentSentProcess.getFutureAction().completeExceptionally(e);
+                this.currentSentProcess = null;
+            }
+            if(this.currentReceiveProcess != null){
+                CloudAPI.getInstance().getConsole().error("Error while processing file-read-transfer process for transfer id: " + this.currentReceiveProcess.getTransferId());
+                this.currentReceiveProcess = null;
             }
         }
     }

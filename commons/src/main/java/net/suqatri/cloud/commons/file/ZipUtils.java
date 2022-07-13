a80b185c857bcc102702fc54a810257f5ca04f4a
Suqatri <paul@suqatri.net>
2022-07-13 13:15:58 +0200

    Update ZipUtils.java

--- commons/src/main/java/net/suqatri/cloud/commons/file/ZipUtils.java
+++ commons/src/main/java/net/suqatri/cloud/commons/file/ZipUtils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
@@ -25,6 +26,48 @@ public class ZipUtils {
             }
             zipOutputStream.closeEntry();
             fileInputStream.close();
+        }
+        zipOutputStream.close();
+    }
+
+    private void zipDirectory(File folderToZip, File zipFile) {
+        try {
+            List<String> filesListInDir = new ArrayList<>();
+            populateFilesList(folderToZip, filesListInDir);
+            //now zip files one by one
+            //create ZipOutputStream to write to the zip file
+            FileOutputStream fos = new FileOutputStream(zipFile);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            for(String filePath : filesListInDir){
+                //for ZipEntry we need to keep only relative file path, so we used substring on absolute path
+                ZipEntry ze = new ZipEntry(filePath.substring(folderToZip.getAbsolutePath().length()+1, filePath.length()));
+                zos.putNextEntry(ze);
+                //read the file and write to ZipOutputStream
+                FileInputStream fis = new FileInputStream(filePath);
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, len);
+                }
+                zos.closeEntry();
+                fis.close();
+            }
+            zos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method populates all the files in a directory to a List
+     * @param dir
+     * @throws IOException
+     */
+    private void populateFilesList(File dir, List<String> filesListInDir) throws IOException {
+        for(File file : dir.listFiles()){
+            if(file.isFile()) filesListInDir.add(file.getAbsolutePath());
+            else populateFilesList(file, filesListInDir);
         }
     }
 
@@ -87,9 +130,10 @@ public class ZipUtils {
                 }
                 fileOutputStream.close();
             }
-            zipInputStream.closeEntry();
             zipEntry = zipInputStream.getNextEntry();
         }
+        zipInputStream.closeEntry();
+        zipInputStream.close();
     }
 
 }

64a88298fe50c0c58c5324662d2a99d0ff41691b
Suqatri <paul@suqatri.net>
2022-07-13 13:19:16 +0200

    Create ServiceCommand.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/commands/ServiceCommand.java
@@ -0,0 +1,14 @@
+package net.suqatri.cloud.node.commands;
+
+import net.suqatri.commands.ConsoleCommand;
+
+public class ServiceCommand extends ConsoleCommand {
+
+    /*
+        * /service start <name>
+        * /service stop <name>
+        * /service list
+        * /service info <name>
+     */
+
+}

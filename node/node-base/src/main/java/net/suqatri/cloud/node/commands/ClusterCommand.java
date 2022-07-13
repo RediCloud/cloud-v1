3f37eefee56640bf2ff6d57392a93571f68e238b
Suqatri <paul@suqatri.net>
2022-07-13 17:52:49 +0200

    Update ClusterCommand.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/commands/ClusterCommand.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/commands/ClusterCommand.java
@@ -27,21 +27,26 @@ public class ClusterCommand extends ConsoleCommand {
 
     @HelpCommand
     @Default
+    @Syntax("[Page]")
+    @Subcommand("help")
+    @Description("Show help for cluster commands")
     public void onHelp(CommandHelp commandHelp){
         commandHelp.showHelp();
     }
 
     @Subcommand("info")
+    @Description("Show information about the cluster")
     public void onInfo(CommandSender commandSender){
         CloudNode node = NodeLauncher.getInstance().getNode();
-        commandSender.sendMessage(NodeLauncher.getInstance().getConsole().getTextColor() + node.getName() + " §7[§f" + node.getUniqueId() + "§7]: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getName());
-        commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Last-IP: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getHostname());
-        commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Up-Time: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getUpTime());
-        commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Services: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getStartedServiceUniqueIds().size());
+        commandSender.sendMessage("%tc" + node.getName() + " §7[%hc" + node.getUniqueId() + "§7]: %hc" + node.getName());
+        commandSender.sendMessage("§8   » %tcLast-IP: %hc"+ node.getHostname());
+        commandSender.sendMessage("§8   » %tcUp-Time: %hc" + node.getUpTime());
+        commandSender.sendMessage("§8   » %tcServices: %hc" + node.getStartedServiceUniqueIds().size());
     }
 
     @Subcommand("info")
     @Syntax("<Node>")
+    @Description("Show information about a specific node")
     public void onInfoNode(CommandSender commandSender, String nodeName){
         commandSender.sendMessage("Loading node...");
         CloudAPI.getInstance().getNodeManager().getNodeAsync(nodeName)
@@ -49,37 +54,39 @@ public class ClusterCommand extends ConsoleCommand {
             .onSuccess(nodeHolder -> {
                 CloudNode node = nodeHolder.getImpl(CloudNode.class);
                 if(node.isConnected()){
-                    commandSender.sendMessage(NodeLauncher.getInstance().getConsole().getTextColor() + node.getName() + " §7[§f" + node.getUniqueId() + "§7]: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getName());
-                    commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Last-IP: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getHostname());
-                    commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Up-Time: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getUpTime());
-                    commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Services: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getStartedServiceUniqueIds().size());
+                    commandSender.sendMessage("%tc" + node.getName() + " §7[§f" + node.getUniqueId() + "§7]: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getName());
+                    commandSender.sendMessage("§8   » %tcLast-IP: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getHostname());
+                    commandSender.sendMessage("§8   » %tcUp-Time: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getUpTime());
+                    commandSender.sendMessage("§8   » %tcServices: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getStartedServiceUniqueIds().size());
                 }else{
-                    commandSender.sendMessage("§8➤ " + NodeLauncher.getInstance().getConsole().getTextColor() + node.getName() + " §7[§f" + node.getUniqueId() + "§7]: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + "Offline");
+                    commandSender.sendMessage("§8➤ %tc" + node.getName() + " §7[%hc" + node.getUniqueId() + "§7]: %hcOffline");
                 }
             });
     }
 
     @Subcommand("shutdown")
+    @Description("Shutdown the cluster")
     public void onShutdown(CommandSender commandSender){
         ((CloudNodeManager)NodeLauncher.getInstance().getNodeManager()).shutdownClusterAsync();
     }
 
     @Subcommand("nodes")
+    @Description("Show all nodes in the cluster")
     public void onNodes(CommandSender commandSender){
         commandSender.sendMessage("Loading nodes...");
         CloudAPI.getInstance().getNodeManager().getNodesAsync()
-                .onFailure(e -> CloudAPI.getInstance().getConsole().error("Failed to get nodes", (Throwable) e))
+                .onFailure(e -> CloudAPI.getInstance().getConsole().error("§cFailed to get nodes", (Throwable) e))
                 .onSuccess(nodes -> {
                     List<ICloudNode> connectedNodes = nodes.parallelStream().map(IRBucketHolder::get).filter(ICloudNode::isConnected).collect(Collectors.toList());
                     List<ICloudNode> offlineNodes = nodes.parallelStream().map(IRBucketHolder::get).filter(node -> !node.isConnected()).collect(Collectors.toList());
                     commandSender.sendMessage("");
-                    commandSender.sendMessage("Connected nodes: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + connectedNodes.size());
-                    commandSender.sendMessage("Offline nodes: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + offlineNodes.size());
+                    commandSender.sendMessage("Connected nodes: %hc" + connectedNodes.size());
+                    commandSender.sendMessage("Offline nodes: %hc" + offlineNodes.size());
                     for(ICloudNode node : connectedNodes){
-                        commandSender.sendMessage(NodeLauncher.getInstance().getConsole().getTextColor() + node.getName() + " §7[§f" + node.getUniqueId() + "§7]: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getName());
-                        commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Last-IP: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getHostname());
-                        commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Up-Time: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getUpTime());
-                        commandSender.sendMessage("§8   » " + NodeLauncher.getInstance().getConsole().getTextColor() + "Services: " + NodeLauncher.getInstance().getConsole().getHighlightColor() + node.getStartedServicesCount());
+                        commandSender.sendMessage("%tc" + node.getName() + " §7[%hc" + node.getUniqueId() + "§7]: %hc" + node.getName());
+                        commandSender.sendMessage("§8   » %tcLast-IP: %gc" + node.getHostname());
+                        commandSender.sendMessage("§8   » %tcUp-Time: %hc" + node.getUpTime());
+                        commandSender.sendMessage("§8   » %tcServices: %hc" + node.getStartedServicesCount());
                     }
                     commandSender.sendMessage("");
                 });

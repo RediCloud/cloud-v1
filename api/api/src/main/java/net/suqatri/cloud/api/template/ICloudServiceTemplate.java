727ddc47d3392b8b886145b014e63b799b0a15f1
Suqatri <paul@suqatri.net>
2022-07-13 13:15:48 +0200

    Update ICloudServiceTemplate.java

--- api/api/src/main/java/net/suqatri/cloud/api/template/ICloudServiceTemplate.java
+++ api/api/src/main/java/net/suqatri/cloud/api/template/ICloudServiceTemplate.java
@@ -1,9 +1,11 @@
 package net.suqatri.cloud.api.template;
 
+import net.suqatri.cloud.api.redis.bucket.IRBucketObject;
+
 import java.io.File;
 import java.io.Serializable;
 
-public interface ICloudServiceTemplate extends Serializable {
+public interface ICloudServiceTemplate extends IRBucketObject {
 
     String getName();
     File getTemplateFolder();

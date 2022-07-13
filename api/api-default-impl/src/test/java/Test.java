19cd85a75b02020bcabf451dc77b6d6a90ef3e24
Suqatri <paul@suqatri.net>
2022-07-13 13:14:19 +0200

    Update Test.java

--- api/api-default-impl/src/test/java/Test.java
+++ api/api-default-impl/src/test/java/Test.java
@@ -7,26 +7,13 @@ import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 public class Test {
 
     public static void main(String[] args) {
-        Config config = new Config();
-        config.useSingleServer()
-                .setAddress("redis://37.114.60.112:6379")
-                .setPassword("MV8kF7VEabLvmTwSx")
-                .setDatabase(2);
-        RedissonClient redissonClient = Redisson.create(config);
 
-        RTopic topic = redissonClient.getTopic("cloud:test", new JsonJacksonCodec());
-        TestPacket testPacket = new TestPacket();
-        testPacket.setUniqueId(UUID.randomUUID());
-        topic.publish(testPacket);
-        System.out.println("Published packet: " + testPacket.getUniqueId());
-        topic.addListener(TestPacket.class, (charSequence, testPacket1) -> {
-            System.out.println("Received Packet: " + testPacket1.getUniqueId());
-        });
     }
 
 }

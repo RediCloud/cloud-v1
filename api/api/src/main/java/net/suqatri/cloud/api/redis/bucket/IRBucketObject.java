fb3800d8d5eb2bcfd50e5bd3bcb22d30e7ec72ac
Suqatri <paul@suqatri.net>
2022-07-13 13:18:57 +0200

    Update IRBucketObject.java

--- api/api/src/main/java/net/suqatri/cloud/api/redis/bucket/IRBucketObject.java
+++ api/api/src/main/java/net/suqatri/cloud/api/redis/bucket/IRBucketObject.java
@@ -1,10 +1,11 @@
 package net.suqatri.cloud.api.redis.bucket;
 
+import net.suqatri.cloud.api.redis.IRUpdateAble;
 import net.suqatri.cloud.commons.function.future.FutureAction;
 
 import java.io.Serializable;
 
-public interface IRBucketObject extends Serializable {
+public interface IRBucketObject extends Serializable, IRUpdateAble {
 
     IRBucketHolder<IRBucketObject> getHolder();
 

c6df33da904ac4ed5b74285720f7505b5baf26f6
Suqatri <paul@suqatri.net>
2022-07-13 17:52:43 +0200

    Update Files.java

--- commons/src/main/java/net/suqatri/cloud/commons/file/Files.java
+++ commons/src/main/java/net/suqatri/cloud/commons/file/Files.java
@@ -8,22 +8,22 @@ import java.io.File;
 @AllArgsConstructor @Getter
 public enum Files {
 
-    CLOUD_FOLDER("./"),
-    MODULES_FOLDER("./modules"),
-    STORAGE_FOLDER("./storage"),
-    TEMPLATE_FOLDER("./templates"),
-    TEMP_FOLDER("./temp"),
-    LIBS_FOLDER("./storage/libs"),
-    LIBS_BLACKLIST_FOLDER("./storage/libs/blacklist"),
-    LIBS_REPO_FOLDER("./storage/libs/repo"),
-    LIBS_INFO_FOLDER("./storage/libs/info"),
-    RUNNER_JAR("./runner.jar"),
-    NODE_JAR("./storage/node.jar"),
-    MINECRAFT_PLUGIN_JAR("./storage/plugin-minecraft.jar"),
-    PROXY_PLUGIN_JAR("./storage/plugin-proxy.jar"),
-    NODE_JSON("./node.json"),
-    VERSIONS_FOLDER("./storage/versions"),
-    REDIS_CONFIG("./storage/redis.json");
+    CLOUD_FOLDER(""),
+    MODULES_FOLDER("modules"),
+    STORAGE_FOLDER("storage"),
+    TEMPLATE_FOLDER("templates"),
+    TEMP_FOLDER("temp"),
+    LIBS_FOLDER("storage/libs"),
+    LIBS_BLACKLIST_FOLDER("storage/libs/blacklist"),
+    LIBS_REPO_FOLDER("storage/libs/repo"),
+    LIBS_INFO_FOLDER("storage/libs/info"),
+    RUNNER_JAR("runner.jar"),
+    NODE_JAR("storage/node.jar"),
+    MINECRAFT_PLUGIN_JAR("storage/plugin-minecraft.jar"),
+    PROXY_PLUGIN_JAR("storage/plugin-proxy.jar"),
+    NODE_JSON("node.json"),
+    VERSIONS_FOLDER("storage/versions"),
+    REDIS_CONFIG("storage/redis.json");
 
     private final String path;
 

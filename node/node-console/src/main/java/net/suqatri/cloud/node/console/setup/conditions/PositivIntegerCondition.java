870c3a93c1e5cfdadf1cf3e0759c9de7897ecc69
Suqatri <paul@suqatri.net>
2022-07-13 17:54:20 +0200

    Create PositivIntegerCondition.java

---
+++ node/node-console/src/main/java/net/suqatri/cloud/node/console/setup/conditions/PositivIntegerCondition.java
@@ -0,0 +1,16 @@
+package net.suqatri.cloud.node.console.setup.conditions;
+
+import net.suqatri.cloud.commons.function.BiSupplier;
+
+public class PositivIntegerCondition implements BiSupplier<String, Boolean> {
+
+    @Override
+    public Boolean supply(String s) {
+        try {
+            int i = Integer.parseInt(s);
+            return i > -1;
+        }catch (Exception e){
+            return false;
+        }
+    }
+}

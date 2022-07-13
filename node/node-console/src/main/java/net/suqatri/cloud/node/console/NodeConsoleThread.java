0b888fde1a10a335517f73babf607def82578274
Suqatri <paul@suqatri.net>
2022-07-13 17:54:17 +0200

    Add unknown command message an console

--- node/node-console/src/main/java/net/suqatri/cloud/node/console/NodeConsoleThread.java
+++ node/node-console/src/main/java/net/suqatri/cloud/node/console/NodeConsoleThread.java
@@ -78,5 +78,6 @@ public class NodeConsoleThread extends Thread {
                 return;
             }
         }
+        this.nodeConsole.commandResponse("%tcUnknown command: %hc" + raw);
     }
 }

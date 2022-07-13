5c8b2d9056299105081397ae3b98cfab802e504e
Suqatri <paul@suqatri.net>
2022-07-13 13:18:53 +0200

    Update ICloudOfflinePlayer.java

--- api/api/src/main/java/net/suqatri/cloud/api/player/ICloudOfflinePlayer.java
+++ api/api/src/main/java/net/suqatri/cloud/api/player/ICloudOfflinePlayer.java
@@ -1,9 +1,11 @@
 package net.suqatri.cloud.api.player;
 
+import net.suqatri.cloud.api.redis.bucket.IRBucketObject;
+
 import java.io.Serializable;
 import java.util.UUID;
 
-public interface ICloudOfflinePlayer extends Serializable {
+public interface ICloudOfflinePlayer extends IRBucketObject {
 
     UUID getUniqueId();
     String getName();

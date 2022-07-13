a4f5ec8883207954c1403be17a83594b0084aeb0
Suqatri <paul@suqatri.net>
2022-07-13 13:16:13 +0200

    Update FileTransferManager.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/file/FileTransferManager.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/FileTransferManager.java
@@ -2,9 +2,8 @@ package net.suqatri.cloud.node.file;
 
 import lombok.Getter;
 import net.suqatri.cloud.api.CloudAPI;
-import net.suqatri.cloud.api.network.INetworkComponentInfo;
-import net.suqatri.cloud.api.network.NetworkComponentType;
 import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.cloud.commons.function.future.FutureAction;
 import net.suqatri.cloud.node.file.packet.FileTransferBytesPacket;
 import net.suqatri.cloud.node.file.packet.FileTransferCompletedPacket;
@@ -13,18 +12,17 @@ import net.suqatri.cloud.node.file.process.FileTransferReceiveProcess;
 import net.suqatri.cloud.node.file.process.FileTransferSentProcess;
 
 import java.io.File;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 
 @Getter
 public class FileTransferManager {
 
-    private int bytesPerChunk = 104857600;
-    private int sleepTimePerPacket = 500;
+    public static final int BYTES_PER_PACKET = 10485760;
+    public static final int SLEEP_TIME_PER_PACKET = 60;
+
     private FileTransferProcessThread thread;
-    private ConcurrentHashMap<UUID, FileTransferReceiveProcess> waitingReceiveProcesses;
+    private final ConcurrentHashMap<UUID, FileTransferReceiveProcess> waitingReceiveProcesses;
 
     public FileTransferManager() {
         this.waitingReceiveProcesses = new ConcurrentHashMap<>();
@@ -45,32 +43,63 @@ public class FileTransferManager {
             CloudAPI.getInstance().getConsole().warn("File-read-transfer process with id " + transferId + " is not found or already processed!");
             return;
         }
-        this.thread.getReceiveProcesses().put(transferId, this.waitingReceiveProcesses.get(transferId));
+        FileTransferProcessThread.getReceiveProcesses().put(transferId, this.waitingReceiveProcesses.get(transferId));
     }
 
-    public FutureAction<File> transferFolderToNode(File folder, File targetFile, INetworkComponentInfo componentInfo){
+    public FutureAction<File> transferFolderToNode(File folder, File targetFile, IRBucketHolder<ICloudNode> holder){
         FutureAction<File> futureAction = new FutureAction<>();
-
-        futureAction.orTimeout(15, TimeUnit.SECONDS);
 
         FileTransferSentProcess process = new FileTransferSentProcess();
         process.setOriginalFile(folder);
         process.setDestinationFilePath(targetFile.getPath());
-        process.setReceiver(componentInfo);
-        process.setFutureAction(futureAction);
+        process.setReceiver(holder);
         process.setFutureAction(futureAction);
 
-        this.thread.getSentProcesses().put(process.getTransferId(), process);
+        futureAction.onFailure(t -> FileTransferProcessThread.getSentProcesses().remove(process.getTransferId()));
+
+        FileTransferProcessThread.getSentProcesses().put(process.getTransferId(), process);
 
         return futureAction;
     }
 
-    public FutureAction<File> transferFolderToNode(File folder, File targetFile, ICloudNode node){
-        return this.transferFolderToNode(folder, targetFile, node.getNetworkComponentInfo());
+    public FutureAction<File> transferFolderToNode(File folder, File targetFile, UUID nodeId){
+        FutureAction<File> futureAction = new FutureAction<>();
+        CloudAPI.getInstance().getNodeManager().getNodeAsync(nodeId)
+                .onFailure(futureAction)
+                .onSuccess(nodeHolder -> this.transferFolderToNode(folder, targetFile, nodeHolder)
+                        .onFailure(futureAction)
+                        .onSuccess(futureAction::complete));
+
+        return futureAction;
     }
 
-    public FutureAction<File> transferFolderToNode(File folder, File targetFile, UUID nodeId){
-        return this.transferFolderToNode(folder, targetFile, CloudAPI.getInstance().getNetworkComponentManager().getComponentInfo(NetworkComponentType.NODE, nodeId.toString()));
+    public void cancelTransfer(UUID transferId, boolean interrupt){
+        if(FileTransferProcessThread.getCurrentReceiveProcess() != null){
+            if(FileTransferProcessThread.getCurrentReceiveProcess() != null) {
+                if (FileTransferProcessThread.getCurrentReceiveProcess().getTransferId().equals(transferId)) {
+                    if (interrupt) {
+                        this.thread.interrupt();
+                        this.thread = new FileTransferProcessThread();
+                        this.thread.start();
+                    }
+                    FileTransferProcessThread.getCurrentReceiveProcess().cancel();
+                    FileTransferProcessThread.setCurrentReceiveProcess(null);
+                }
+            }
+        }
+        if(FileTransferProcessThread.getCurrentSentProcess() != null){
+            if(FileTransferProcessThread.getCurrentSentProcess() != null) {
+                if (FileTransferProcessThread.getCurrentSentProcess().getTransferId().equals(transferId)) {
+                    if (interrupt) {
+                        this.thread.interrupt();
+                        this.thread = new FileTransferProcessThread();
+                        this.thread.start();
+                    }
+                    FileTransferProcessThread.getCurrentSentProcess().cancel();
+                    FileTransferProcessThread.setCurrentSentProcess(null);
+                }
+            }
+        }
     }
 
 }

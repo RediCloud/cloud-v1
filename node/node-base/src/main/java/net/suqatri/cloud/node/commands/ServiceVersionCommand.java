e80a123f368c0f56a3f14e682fdcdd692c18963b
Suqatri <paul@suqatri.net>
2022-07-13 17:52:59 +0200

    Create ServiceVersionCommand.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/commands/ServiceVersionCommand.java
@@ -0,0 +1,8 @@
+package net.suqatri.cloud.node.commands;
+
+import net.suqatri.commands.ConsoleCommand;
+import net.suqatri.commands.annotation.CommandAlias;
+
+@CommandAlias("sv|serviceversion|serviceversions")
+public class ServiceVersionCommand extends ConsoleCommand {
+}

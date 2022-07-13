3df49de9d0a4cdb25087efd42aff0d798596ddd0
Suqatri <paul@suqatri.net>
2022-07-13 13:16:40 +0200

    Update FileTransferSentProcess.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/file/process/FileTransferSentProcess.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/file/process/FileTransferSentProcess.java
@@ -2,14 +2,16 @@ package net.suqatri.cloud.node.file.process;
 
 import lombok.Data;
 import net.suqatri.cloud.api.CloudAPI;
-import net.suqatri.cloud.api.network.INetworkComponentInfo;
 import net.suqatri.cloud.api.network.NetworkComponentType;
+import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.cloud.commons.file.FileUtils;
 import net.suqatri.cloud.commons.file.Files;
 import net.suqatri.cloud.commons.file.ZipUtils;
 import net.suqatri.cloud.commons.function.future.FutureAction;
-import net.suqatri.cloud.node.NodeLauncher;
+import net.suqatri.cloud.node.file.FileTransferManager;
 import net.suqatri.cloud.node.file.packet.FileTransferBytesPacket;
+import net.suqatri.cloud.node.file.packet.FileTransferCancelPacket;
 import net.suqatri.cloud.node.file.packet.FileTransferCompletedPacket;
 import net.suqatri.cloud.node.file.packet.FileTransferStartPacket;
 
@@ -20,7 +22,7 @@ import java.util.UUID;
 public class FileTransferSentProcess implements IFileTransferProcess {
 
     private final UUID transferId = UUID.randomUUID();
-    private INetworkComponentInfo receiver;
+    private IRBucketHolder<ICloudNode> receiver;
 
     private File originalFile;
     private String destinationFilePath;
@@ -28,23 +30,28 @@ public class FileTransferSentProcess implements IFileTransferProcess {
     private FutureAction<File> futureAction;
     private File zipFile;
 
+    private boolean startPacketSent = false;
+
+
     @Override
     public void process() {
         try{
 
-            if(this.receiver.getType() == NetworkComponentType.SERVICE){
+            if(this.receiver.get().getNetworkComponentInfo().getType() == NetworkComponentType.SERVICE){
                 this.futureAction.completeExceptionally(new IllegalArgumentException("Cannot send file to service!"));
                 return;
             }
 
             this.zipFile = new File(Files.TEMP_FOLDER.getFile(), this.transferId + ".zip");
 
-            ZipUtils.zipDirFiles(this.originalFile, FileUtils.getAllFilesAndDirs(this.originalFile), this.zipFile);
+            ZipUtils.zipDir(this.originalFile, this.zipFile);
+
+            futureAction.onFailure(e -> this.cancel());
 
             byte[] sentData = FileUtils.fileToBytes(this.zipFile.getAbsolutePath());
             int size = sentData.length;
-            int packetCount = size / NodeLauncher.getInstance().getFileTransferManager().getBytesPerChunk();
-            if(size % NodeLauncher.getInstance().getFileTransferManager().getBytesPerChunk() != 0){
+            int packetCount = size / FileTransferManager.BYTES_PER_PACKET;
+            if(size % FileTransferManager.BYTES_PER_PACKET != 0){
                 packetCount++;
             }
             FileTransferStartPacket startPacket = new FileTransferStartPacket();
@@ -52,30 +59,44 @@ public class FileTransferSentProcess implements IFileTransferProcess {
             startPacket.setDestinationFilePath(this.destinationFilePath);
             startPacket.setTransferId(this.transferId);
             startPacket.setIndexes(packetCount);
-            startPacket.getPacketData().addReceiver(this.receiver);
-            startPacket.publish();
+            startPacket.getPacketData().addReceiver(this.receiver.get().getNetworkComponentInfo());
+            startPacket.publishAsync();
+            this.startPacketSent = true;
 
             int index = 0;
-            int packetSize = NodeLauncher.getInstance().getFileTransferManager().getBytesPerChunk();
-            while(index < sentData.length){
-                byte[] data = new byte[packetSize];
-                System.arraycopy(sentData, index, data, 0, packetSize);
+            int count = 0;
+            int packetSize = FileTransferManager.BYTES_PER_PACKET;
+            while(count < sentData.length){
+                Thread.sleep(FileTransferManager.SLEEP_TIME_PER_PACKET);
+                byte[] data;
+                if(sentData.length - count < packetSize) {
+                    data = new byte[sentData.length - count];
+                    System.arraycopy(sentData, count, data, 0, sentData.length - count);
+                }else{
+                    data = new byte[packetSize];
+                    System.arraycopy(sentData, count, data, 0, packetSize);
+                }
+                if(!this.receiver.get().isConnected()){
+                    cancel();
+                    return;
+                }
                 FileTransferBytesPacket packet = new FileTransferBytesPacket();
                 packet.setTransferId(this.transferId);
-                packet.setIndexId(index);
+                packet.setIndexId(count);
                 packet.setFileData(data);
-                packet.getPacketData().addReceiver(this.receiver);
-                packet.publish();
+                packet.getPacketData().addReceiver(this.receiver.get().getNetworkComponentInfo());
+                packet.publishAsync();
                 CloudAPI.getInstance().getConsole().debug("Send FileTransferBytesPacket with index " + index + " and transferId: " + this.transferId + " (Byte-Size: " + data.length + ")");
-                Thread.sleep(NodeLauncher.getInstance().getFileTransferManager().getSleepTimePerPacket());
-                index += packetSize;
+                count += packetSize;
+                index++;
             }
 
-            Thread.sleep(NodeLauncher.getInstance().getFileTransferManager().getSleepTimePerPacket());
+            Thread.sleep(1500);
+
             FileTransferCompletedPacket completedPacket = new FileTransferCompletedPacket();
             completedPacket.setTransferId(this.transferId);
-            completedPacket.getPacketData().addReceiver(this.receiver);
-            completedPacket.publish();
+            completedPacket.getPacketData().addReceiver(this.receiver.get().getNetworkComponentInfo());
+            completedPacket.publishAsync();
 
             this.zipFile.delete();
 
@@ -83,5 +104,25 @@ public class FileTransferSentProcess implements IFileTransferProcess {
         }catch (Exception e){
             this.futureAction.completeExceptionally(e);
         }
+    }
+
+    @Override
+    public void cancel() {
+        if(this.zipFile != null) {
+            if(this.zipFile.exists()) this.zipFile.delete();
+        }
+        if(this.futureAction.isDone()) return;
+        if(!this.startPacketSent) return;
+        FileTransferCancelPacket cancelPacket = new FileTransferCancelPacket();
+        cancelPacket.setTransferId(this.transferId);
+        cancelPacket.getPacketData().addReceiver(this.receiver.get().getNetworkComponentInfo());
+        cancelPacket.publishAsync();
+    }
+
+    public static enum FileTransferSentProcessState {
+        STARTED_ZIPPING,
+        STARTED_SENDING,
+        COMPLETED,
+        CANCELED
     }
 }

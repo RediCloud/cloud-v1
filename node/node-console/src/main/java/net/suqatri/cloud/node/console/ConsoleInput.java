d0c22c8b3d6a7a3442f6088474e9442ca514ee55
Suqatri <paul@suqatri.net>
2022-07-14 13:39:12 +0200

    Create ConsoleInput.java

---
+++ node/node-console/src/main/java/net/suqatri/cloud/node/console/ConsoleInput.java
@@ -0,0 +1,18 @@
+package net.suqatri.cloud.node.console;
+
+import lombok.Data;
+import net.suqatri.cloud.api.console.IConsoleInput;
+import net.suqatri.commands.ConsoleCommandManager;
+
+@Data
+public class ConsoleInput implements IConsoleInput {
+
+    private final String input;
+    private final long time;
+    private final String prefix;
+
+    @Override
+    public void logAsFake() {
+        ((CommandConsoleManager)CommandConsoleManager.getCurrentCommandManager()).getNodeConsole().log(new ConsoleLine("", prefix + input).setPrintTimestamp(false).setPrintPrefix(false));
+    }
+}

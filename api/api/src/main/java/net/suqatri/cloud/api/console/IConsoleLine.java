b5294b6b0daccc21ee3c61a794833a8d16192df7
Suqatri <paul@suqatri.net>
2022-07-14 13:38:46 +0200

    Update IConsoleLine.java

--- api/api/src/main/java/net/suqatri/cloud/api/console/IConsoleLine.java
+++ api/api/src/main/java/net/suqatri/cloud/api/console/IConsoleLine.java
@@ -1,10 +1,23 @@
 package net.suqatri.cloud.api.console;
 
-public interface IConsoleLine {
+public interface IConsoleLine extends IConsoleLineEntry {
 
     String getPrefix();
+    boolean printPrefix();
+    IConsoleLine setPrintPrefix(boolean printPrefix);
+
     String getMessage();
+
     LogLevel getLogLevel();
+
     void println();
+
+    long getTime();
+
+    boolean printTimestamp();
+    IConsoleLine setPrintTimestamp(boolean printTimestamp);
+
+    IConsoleLine setStored(boolean stored);
+    boolean isStored();
 
 }

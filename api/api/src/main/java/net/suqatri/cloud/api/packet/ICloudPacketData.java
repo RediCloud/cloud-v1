8dd89753096c3106587a922e03f828a8145fa97f
Suqatri <paul@suqatri.net>
2022-07-14 22:05:25 +0200

    Update ICloudPacketData.java

--- api/api/src/main/java/net/suqatri/cloud/api/packet/ICloudPacketData.java
+++ api/api/src/main/java/net/suqatri/cloud/api/packet/ICloudPacketData.java
@@ -1,9 +1,11 @@
 package net.suqatri.cloud.api.packet;
 
 import net.suqatri.cloud.api.network.INetworkComponentInfo;
+import net.suqatri.cloud.commons.function.future.FutureAction;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.UUID;
 
 public interface ICloudPacketData extends Serializable{
 
@@ -13,6 +15,13 @@ public interface ICloudPacketData extends Serializable{
     ICloudPacketData removeReceiver(INetworkComponentInfo receiver);
     ICloudPacketData removeReceivers(INetworkComponentInfo... receivers);
     ICloudPacketData clearReceivers();
+
+    FutureAction<ICloudPacket> waitForResponse();
+    UUID getPacketId();
+    UUID getResponseTargetId();
+    ICloudPacketData getResponseTargetData();
+    void setResponseTargetData(ICloudPacketData packetData);
+    FutureAction<ICloudPacket> getResponseAction();
 
     ICloudPacketData allowSenderAsReceiver();
     boolean isSenderAsReceiverAllowed();

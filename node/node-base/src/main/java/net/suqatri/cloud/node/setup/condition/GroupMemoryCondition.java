3e9b38051285fc0b3b88666f4d29fda34f9ca5ae
Suqatri <paul@suqatri.net>
2022-07-14 14:24:34 +0200

    Create GroupMemoryCondition.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/setup/condition/GroupMemoryCondition.java
@@ -0,0 +1,16 @@
+package net.suqatri.cloud.node.setup.condition;
+
+import net.suqatri.cloud.commons.ConditionChecks;
+import net.suqatri.cloud.commons.function.BiSupplier;
+import net.suqatri.cloud.node.console.setup.annotations.ConditionChecker;
+
+import java.lang.annotation.Annotation;
+
+public class GroupMemoryCondition implements BiSupplier<String, Boolean> {
+
+    @Override
+    public Boolean supply(String s) {
+        if(!ConditionChecks.isInteger(s)) return false;
+        return Integer.parseInt(s) >= 500;
+    }
+}

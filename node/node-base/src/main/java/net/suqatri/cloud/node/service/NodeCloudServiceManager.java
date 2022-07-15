b8d9c35113f280a60a37a94701a16c384ffca0d4
Suqatri <paul@suqatri.net>
2022-07-15 02:22:46 +0200

    Update NodeCloudServiceManager.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/service/NodeCloudServiceManager.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/service/NodeCloudServiceManager.java
@@ -1,21 +1,34 @@
 package net.suqatri.cloud.node.service;
 
 import net.suqatri.cloud.api.impl.service.CloudServiceManager;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.api.service.ICloudService;
+import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
 import net.suqatri.cloud.commons.function.future.FutureAction;
+import net.suqatri.cloud.node.NodeLauncher;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public class NodeCloudServiceManager extends CloudServiceManager {
 
-    //TODO stop process
     @Override
-    public FutureAction<Boolean> stopServiceAsync(UUID uniqueId) {
-        return super.stopServiceAsync(uniqueId);
+    public FutureAction<Boolean> stopServiceAsync(UUID uniqueId, boolean force) {
+        return NodeLauncher.getInstance().getServiceFactory().destroyServiceAsync(uniqueId, force);
     }
 
-    //TODO stop process
     @Override
-    public boolean stopService(UUID uniqueId) {
-        return super.stopService(uniqueId);
+    public boolean stopService(UUID uniqueId, boolean force) throws Exception {
+        return NodeLauncher.getInstance().getServiceFactory().destroyService(uniqueId, force);
+    }
+
+    @Override
+    public IRBucketHolder<ICloudService> startService(IServiceStartConfiguration configuration) throws Exception {
+        return NodeLauncher.getInstance().getServiceFactory().createService(configuration);
+    }
+
+    @Override
+    public FutureAction<IRBucketHolder<ICloudService>> startServiceAsync(IServiceStartConfiguration configuration) {
+        return NodeLauncher.getInstance().getServiceFactory().createServiceAsync(configuration);
     }
 }

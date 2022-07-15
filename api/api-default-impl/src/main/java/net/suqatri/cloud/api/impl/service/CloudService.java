7ad158611698ab14a1fc4e348b7310d569b51e9a
Suqatri <paul@suqatri.net>
2022-07-15 02:14:27 +0200

    Update CloudService.java

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/service/CloudService.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/service/CloudService.java
@@ -3,12 +3,11 @@ package net.suqatri.cloud.api.impl.service;
 import lombok.Getter;
 import lombok.Setter;
 import net.suqatri.cloud.api.CloudAPI;
-import net.suqatri.cloud.api.group.ICloudGroup;
 import net.suqatri.cloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.cloud.api.impl.redis.bucket.RBucketObject;
 import net.suqatri.cloud.api.network.NetworkComponentType;
 import net.suqatri.cloud.api.service.ICloudService;
-import net.suqatri.cloud.api.service.IServiceStartConfiguration;
+import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
 import net.suqatri.cloud.api.service.ServiceState;
 
 
@@ -26,4 +25,5 @@ public class CloudService extends RBucketObject implements ICloudService {
         if(CloudAPI.getInstance().getApplicationType().getNetworkComponentType() != NetworkComponentType.SERVICE) return;
         CloudDefaultAPIImpl.getInstance().updateApplicationProperties(this);
     }
+
 }

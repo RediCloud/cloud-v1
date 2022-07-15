a984b07b6d9752b409677459888d36cfb9c9022f
Suqatri <paul@suqatri.net>
2022-07-15 02:22:29 +0200

    Create NodePingPacket.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/node/packet/NodePingPacket.java
@@ -0,0 +1,12 @@
+package net.suqatri.cloud.node.node.packet;
+
+import lombok.Getter;
+import net.suqatri.cloud.api.impl.packet.CloudPacket;
+import net.suqatri.cloud.api.impl.packet.response.CloudPacketResponse;
+
+@Getter
+public class NodePingPacket extends CloudPacketResponse {
+
+    private final long time = System.currentTimeMillis();
+
+}

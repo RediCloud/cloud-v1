9580ef8f4f2d8ca883189e023c2c34dace95a39a
Suqatri <paul@suqatri.net>
2022-07-15 02:20:59 +0200

    Create IServiceStartConfiguration.java

---
+++ api/api/src/main/java/net/suqatri/cloud/api/service/configuration/IServiceStartConfiguration.java
@@ -0,0 +1,40 @@
+package net.suqatri.cloud.api.service.configuration;
+
+import net.suqatri.cloud.api.group.ICloudGroup;
+import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.api.service.ServiceEnvironment;
+import net.suqatri.cloud.api.template.ICloudServiceTemplate;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public interface IServiceStartConfiguration {
+
+    ServiceEnvironment getEnvironment();
+    String getJavaCommand();
+
+    String getName();
+    UUID getUniqueId();
+
+    int getId();
+
+    int getMaxMemory();
+
+    Collection<UUID> getPossibleNodeIds();
+
+    int getStartPriority();
+
+    boolean isStatic();
+
+    Collection<String> getTemplatesNames();
+
+    @Nullable
+    IRBucketHolder<ICloudGroup> getGroup();
+
+    List<String> getProcessParameters();
+    List<String> getJvmArguments();
+
+}

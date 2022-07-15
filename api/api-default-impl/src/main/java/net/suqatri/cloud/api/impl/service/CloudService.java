4e474ada28a12a67e97d68b8817e78d05e18936b
Suqatri <paul@suqatri.net>
2022-07-15 17:36:47 +0200

    Update CloudService.java

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/service/CloudService.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/service/CloudService.java
@@ -6,9 +6,12 @@ import net.suqatri.cloud.api.CloudAPI;
 import net.suqatri.cloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.cloud.api.impl.redis.bucket.RBucketObject;
 import net.suqatri.cloud.api.network.NetworkComponentType;
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.cloud.api.service.ICloudService;
 import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
 import net.suqatri.cloud.api.service.ServiceState;
+import net.suqatri.cloud.api.service.version.ICloudServiceVersion;
+import net.suqatri.cloud.commons.function.future.FutureAction;
 
 
 @Getter
@@ -24,6 +27,11 @@ public class CloudService extends RBucketObject implements ICloudService {
     public void merged() {
         if(CloudAPI.getInstance().getApplicationType().getNetworkComponentType() != NetworkComponentType.SERVICE) return;
         CloudDefaultAPIImpl.getInstance().updateApplicationProperties(this);
+    }
+
+    @Override
+    public FutureAction<IRBucketHolder<ICloudServiceVersion>> getServiceVersion() {
+        return CloudAPI.getInstance().getServiceVersionManager().getServiceVersionAsync(this.configuration.getServiceVersionName());
     }
 
 }

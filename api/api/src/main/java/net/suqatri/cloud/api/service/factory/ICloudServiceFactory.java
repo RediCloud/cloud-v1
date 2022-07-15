8d5d8f48912556224e31ff2a51b0ba0ed772d583
Suqatri <paul@suqatri.net>
2022-07-15 02:21:01 +0200

    Update ICloudServiceFactory.java

--- api/api/src/main/java/net/suqatri/cloud/api/service/factory/ICloudServiceFactory.java
+++ api/api/src/main/java/net/suqatri/cloud/api/service/factory/ICloudServiceFactory.java
@@ -1,10 +1,19 @@
 package net.suqatri.cloud.api.service.factory;
 
+import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.cloud.api.service.ICloudService;
-import net.suqatri.cloud.api.service.IServiceStartConfiguration;
+import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
+import net.suqatri.cloud.commons.function.future.FutureAction;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public interface ICloudServiceFactory {
 
-    ICloudService createCloudService(IServiceStartConfiguration configuration);
+    IRBucketHolder<ICloudService> createService(IServiceStartConfiguration configuration) throws Exception;
+    FutureAction<IRBucketHolder<ICloudService>> createServiceAsync(IServiceStartConfiguration configuration);
+
+    boolean destroyService(UUID uniqueId, boolean force) throws IOException;
+    FutureAction<Boolean> destroyServiceAsync(UUID uniqueId, boolean force);
 
 }

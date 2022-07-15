8a09882c37b2786e758384c45d631c294b8289f5
Suqatri <paul@suqatri.net>
2022-07-15 17:37:06 +0200

    Update ICloudService.java

--- api/api/src/main/java/net/suqatri/cloud/api/service/ICloudService.java
+++ api/api/src/main/java/net/suqatri/cloud/api/service/ICloudService.java
@@ -1,15 +1,22 @@
 package net.suqatri.cloud.api.service;
 
+import net.suqatri.cloud.api.CloudAPI;
 import net.suqatri.cloud.api.group.ICloudGroup;
 import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.cloud.api.redis.bucket.IRBucketObject;
 import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
+import net.suqatri.cloud.api.service.version.ICloudServiceVersion;
+import net.suqatri.cloud.commons.function.future.FutureAction;
 
 import java.util.UUID;
 
 public interface ICloudService extends IRBucketObject {
 
     IServiceStartConfiguration getConfiguration();
+
+    default FutureAction<IRBucketHolder<ICloudServiceVersion>> getServiceVersion(){
+        return CloudAPI.getInstance().getServiceVersionManager().getServiceVersionAsync(getConfiguration().getServiceVersionName());
+    }
 
     default ServiceEnvironment getEnvironment(){
         return getConfiguration().getEnvironment();

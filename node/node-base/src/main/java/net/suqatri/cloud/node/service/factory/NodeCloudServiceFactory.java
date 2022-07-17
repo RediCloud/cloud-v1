5721a92ae0419c13f3892c7adc4f208d221d136c
Suqatri <paul@suqatri.net>
2022-07-17 15:36:44 +0200

    Update NodeCloudServiceFactory.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/service/factory/NodeCloudServiceFactory.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/service/factory/NodeCloudServiceFactory.java
@@ -1,17 +1,26 @@
 package net.suqatri.cloud.node.service.factory;
 
 import lombok.Data;
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.cloud.api.impl.service.CloudService;
+import net.suqatri.cloud.api.impl.service.version.CloudServiceVersion;
 import net.suqatri.cloud.api.node.service.factory.ICloudNodeServiceFactory;
 import net.suqatri.cloud.api.node.service.factory.ICloudServiceProcess;
 import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.api.redis.event.RedisConnectedEvent;
 import net.suqatri.cloud.api.service.ICloudService;
 import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
 import net.suqatri.cloud.api.service.ServiceState;
+import net.suqatri.cloud.api.service.version.ICloudServiceVersion;
 import net.suqatri.cloud.commons.function.future.FutureAction;
 import net.suqatri.cloud.node.service.NodeCloudServiceManager;
+import org.redisson.api.RLock;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,17 +28,38 @@ import java.util.concurrent.ConcurrentHashMap;
 public class NodeCloudServiceFactory implements ICloudNodeServiceFactory {
 
     private final NodeCloudServiceManager serviceManager;
-    private final CloudNodePortManager portManager = new CloudNodePortManager();
+    private final CloudNodePortManager portManager;
+    private RLock lock;
 
-    private final ConcurrentHashMap<UUID, CloudServiceServiceProcess> processes = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<UUID, CloudServiceServiceProcess> processes;
+
+    public NodeCloudServiceFactory(NodeCloudServiceManager serviceManager) {
+        this.serviceManager = serviceManager;
+        this.portManager = new CloudNodePortManager();
+        this.processes = new ConcurrentHashMap<>();
+        CloudAPI.getInstance().getEventManager().register(RedisConnectedEvent.class, event -> this.lock = CloudDefaultAPIImpl.getInstance().getRedisConnection().getClient().getLock("cloud:service-factory:start"));
+    }
 
     @Override
     public IRBucketHolder<ICloudService> createService(IServiceStartConfiguration configuration) throws Exception{
-        for (IRBucketHolder<ICloudService> serviceHolder : this.serviceManager.getServices()) {
+
+        if(!CloudAPI.getInstance().getServiceVersionManager().existsServiceVersion(configuration.getServiceVersionName())) throw new Exception("Service version " + configuration.getServiceVersionName() + " not found");
+        IRBucketHolder<ICloudServiceVersion> versionHolder = CloudAPI.getInstance().getServiceVersionManager().getServiceVersion(configuration.getServiceVersionName());
+        if(!versionHolder.get().isDownloaded()) versionHolder.getImpl(CloudServiceVersion.class).download();
+        if(versionHolder.get().needPatch()) versionHolder.getImpl(CloudServiceVersion.class).patch();
+
+        Collection<IRBucketHolder<ICloudService>> serviceHolders = this.serviceManager.getServices();
+
+        if(configuration.getId() < 1) {
+            configuration.setId(this.getNextId(configuration.getName(), this.serviceManager.getServices()));
+        }
+
+        for (IRBucketHolder<ICloudService> serviceHolder : serviceHolders) {
             if(serviceHolder.get().getServiceName().equalsIgnoreCase(configuration.getName() + "-" + configuration.getId())) {
-                return serviceHolder;
+                throw new IllegalArgumentException("Service " + configuration.getName() + "-" + configuration.getId() + " already exists");
             }
         }
+
         CloudService cloudService = new CloudService();
         cloudService.setConfiguration(configuration);
         cloudService.setServiceState(ServiceState.STARTING);
@@ -49,29 +79,50 @@ public class NodeCloudServiceFactory implements ICloudNodeServiceFactory {
 
         this.serviceManager.getServicesAsync()
             .onFailure(futureAction)
-            .onSuccess(services -> {
-                for (IRBucketHolder<ICloudService> serviceHolder : services) {
+            .onSuccess(serviceHolders -> {
+
+                if(configuration.getId() < 1) {
+                    configuration.setId(this.getNextId(configuration.getName(), serviceHolders));
+                }
+
+                for (IRBucketHolder<ICloudService> serviceHolder : serviceHolders) {
                     if(serviceHolder.get().getServiceName().equalsIgnoreCase(configuration.getName() + "-" + configuration.getId())){
-                        futureAction.completeExceptionally(new IllegalStateException("Service already exists"));
+                        futureAction.completeExceptionally(new IllegalArgumentException("Service " + configuration.getName() + "-" + configuration.getId() + " already exists"));
                         return;
                     }
                 }
-                CloudService cloudService = new CloudService();
-                cloudService.setConfiguration(configuration);
-                cloudService.setServiceState(ServiceState.PREPARE);
-                cloudService.setMaxPlayers(50);
-                cloudService.setMotd("Welcome to Suqatri Cloud");
-                this.serviceManager.createBucketAsync(cloudService.getUniqueId().toString(), cloudService)
-                    .onFailure(futureAction)
-                    .onSuccess(serviceHolder -> {
 
-                        CloudServiceServiceProcess process = new CloudServiceServiceProcess(this, serviceHolder);
-                        process.startAsync()
-                            .onFailure(futureAction)
-                            .onSuccess(s -> {
-                                futureAction.complete(serviceHolder);
-                            });
-                    });
+                CloudAPI.getInstance().getServiceVersionManager().existsServiceVersionAsync(configuration.getServiceVersionName())
+                        .onFailure(futureAction)
+                        .onSuccess(existVersion -> {
+
+                            if(!existVersion){
+                                futureAction.completeExceptionally(new IllegalStateException("Service version " + configuration.getServiceVersionName() + " not found"));
+                                return;
+                            }
+
+                            IRBucketHolder<ICloudServiceVersion> versionHolder = CloudAPI.getInstance().getServiceVersionManager().getServiceVersion(configuration.getServiceVersionName());
+                            versionHolder.get().getPatchedFileAsync(true)
+                                    .onFailure(futureAction)
+                                    .onSuccess(versionFile -> {
+                                        CloudService cloudService = new CloudService();
+                                        cloudService.setConfiguration(configuration);
+                                        cloudService.setServiceState(ServiceState.PREPARE);
+                                        cloudService.setMaxPlayers(50);
+                                        cloudService.setMotd("Welcome to Suqatri Cloud");
+                                        this.serviceManager.createBucketAsync(cloudService.getUniqueId().toString(), cloudService)
+                                                .onFailure(futureAction)
+                                                .onSuccess(serviceHolder -> {
+
+                                                    CloudServiceServiceProcess process = new CloudServiceServiceProcess(this, serviceHolder);
+                                                    process.startAsync()
+                                                            .onFailure(futureAction)
+                                                            .onSuccess(s -> {
+                                                                futureAction.complete(serviceHolder);
+                                                            });
+                                                });
+                                    });
+                        });
             });
 
         return futureAction;
@@ -118,4 +169,17 @@ public class NodeCloudServiceFactory implements ICloudNodeServiceFactory {
 
         return futureAction;
     }
+
+    private int getNextId(String groupName, Collection<IRBucketHolder<ICloudService>> servicesHolders){
+        int i = 1;
+        List<Integer> ids = new ArrayList<>();
+        for (IRBucketHolder<ICloudService> serviceHolder : servicesHolders) {
+            if(serviceHolder.get().getGroupName().equalsIgnoreCase(groupName)){
+                ids.add(serviceHolder.get().getId());
+            }
+        }
+        while (ids.contains(i)) i++;
+        return i;
+    }
+
 }

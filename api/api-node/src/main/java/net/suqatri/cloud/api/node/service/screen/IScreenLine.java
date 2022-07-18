cc35071dc3a316851cf8c8fc665d19bc58f97cbd
Suqatri <paul@suqatri.net>
2022-07-18 18:09:43 +0200

    Add createConsoleLine to IConsoleLine

--- api/api-node/src/main/java/net/suqatri/cloud/api/node/service/screen/IScreenLine.java
+++ api/api-node/src/main/java/net/suqatri/cloud/api/node/service/screen/IScreenLine.java
@@ -1,5 +1,7 @@
 package net.suqatri.cloud.api.node.service.screen;
 
+import net.suqatri.cloud.api.console.IConsoleLine;
+
 import java.io.Serializable;
 
 public interface IScreenLine extends Serializable {
@@ -7,5 +9,6 @@ public interface IScreenLine extends Serializable {
     long getTime();
     String getLine();
     void print();
+    IConsoleLine createConsoleLine();
 
 }

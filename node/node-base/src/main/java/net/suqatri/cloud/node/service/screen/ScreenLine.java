d260d1561bc950deec15630ec39dad85f007a725
Suqatri <paul@suqatri.net>
2022-07-18 18:11:19 +0200

    Update ScreenLine.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/service/screen/ScreenLine.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/service/screen/ScreenLine.java
@@ -2,6 +2,8 @@ package net.suqatri.cloud.node.service.screen;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import net.suqatri.cloud.api.CloudAPI;
+import net.suqatri.cloud.api.console.IConsoleLine;
 import net.suqatri.cloud.api.node.service.screen.IScreenLine;
 import net.suqatri.cloud.node.console.ConsoleLine;
 
@@ -13,11 +15,26 @@ public class ScreenLine implements IScreenLine {
     private final long time = System.currentTimeMillis();
     private final String line;
 
+    public ScreenLine(){
+        this.line = null;
+        this.screen = null;
+    }
+
+    public ScreenLine(ServiceScreen screen, String line){
+        this.screen = screen;
+        this.line = line;
+    }
+
     @Override
-    public void print() {
+    public IConsoleLine createConsoleLine() {
         ConsoleLine consoleLine = new ConsoleLine("SCREEN [" + screen.getService().get().getServiceName() + "]", line);
         consoleLine.setTime(time);
         consoleLine.setStored(false);
-        consoleLine.println();
+        return consoleLine;
+    }
+
+    @Override
+    public void print() {
+        CloudAPI.getInstance().getConsole().log(createConsoleLine());
     }
 }

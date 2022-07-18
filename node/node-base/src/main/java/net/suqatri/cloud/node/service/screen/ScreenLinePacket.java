016bd02f4bfebfd4b47cd7bf62083591e7b9d021
Suqatri <paul@suqatri.net>
2022-07-18 16:30:51 +0200

    Create ScreenLinePacket.java

---
+++ node/node-base/src/main/java/net/suqatri/cloud/node/service/screen/ScreenLinePacket.java
@@ -0,0 +1,21 @@
+package net.suqatri.cloud.node.service.screen;
+
+import lombok.Data;
+import net.suqatri.cloud.api.impl.packet.CloudPacket;
+import net.suqatri.cloud.node.NodeLauncher;
+
+import java.util.UUID;
+
+@Data
+public class ScreenLinePacket extends CloudPacket {
+
+    private ScreenLine screenLine;
+    private UUID serviceId;
+
+    @Override
+    public void receive() {
+        if(NodeLauncher.getInstance().getConsole().getCurrentSetup() != null) return;
+        if(!NodeLauncher.getInstance().getScreenManager().isActive(serviceId)) return;
+        screenLine.print();
+    }
+}

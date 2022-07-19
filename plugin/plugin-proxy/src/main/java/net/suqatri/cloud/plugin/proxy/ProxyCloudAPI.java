eee424dce1a00f586ffdb5474597f18309515149
Suqatri <paul@suqatri.net>
2022-07-19 19:20:12 +0200

    Create ProxyCloudAPI.java

---
+++ plugin/plugin-proxy/src/main/java/net/suqatri/cloud/plugin/proxy/ProxyCloudAPI.java
@@ -0,0 +1,154 @@
+package net.suqatri.cloud.plugin.proxy;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.scheduler.ScheduledTask;
+import net.suqatri.cloud.api.console.ICommandManager;
+import net.suqatri.cloud.api.console.IConsole;
+import net.suqatri.cloud.api.impl.CloudDefaultAPIImpl;
+import net.suqatri.cloud.api.impl.redis.RedisConnection;
+import net.suqatri.cloud.api.impl.service.CloudService;
+import net.suqatri.cloud.api.impl.service.CloudServiceManager;
+import net.suqatri.cloud.api.impl.service.factory.CloudServiceFactory;
+import net.suqatri.cloud.api.impl.service.version.CloudServiceVersionManager;
+import net.suqatri.cloud.api.impl.template.CloudServiceTemplateManager;
+import net.suqatri.cloud.api.network.INetworkComponentInfo;
+import net.suqatri.cloud.api.player.ICloudPlayerManager;
+import net.suqatri.cloud.api.redis.IRedisConnection;
+import net.suqatri.cloud.api.redis.RedisCredentials;
+import net.suqatri.cloud.api.scheduler.IScheduler;
+import net.suqatri.cloud.api.service.ICloudServiceManager;
+import net.suqatri.cloud.api.service.ServiceState;
+import net.suqatri.cloud.api.service.factory.ICloudServiceFactory;
+import net.suqatri.cloud.api.service.version.ICloudServiceVersionManager;
+import net.suqatri.cloud.api.template.ICloudServiceTemplateManager;
+import net.suqatri.cloud.api.utils.ApplicationType;
+import net.suqatri.cloud.api.utils.Files;
+import net.suqatri.cloud.commons.file.FileWriter;
+import net.suqatri.cloud.plugin.proxy.console.ProxyConsole;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Getter
+public class ProxyCloudAPI extends CloudDefaultAPIImpl<CloudService> {
+
+    private final Plugin plugin;
+    private CloudService service;
+    private RedisConnection redisConnection;
+    private final CloudServiceFactory serviceFactory;
+    private final CloudServiceManager serviceManager;
+    private final CloudServiceVersionManager serviceVersionManager;
+    private final CloudServiceTemplateManager serviceTemplateManager;
+    private ScheduledTask updaterTask;
+    private final ProxyConsole console;
+
+    public ProxyCloudAPI(Plugin plugin) {
+        super(ApplicationType.SERVICE_PROXY);
+        this.plugin = plugin;
+        this.console = new ProxyConsole(this.plugin.getLogger());
+        this.serviceManager = new CloudServiceManager();
+        this.serviceFactory = new CloudServiceFactory(this.serviceManager);
+        this.serviceVersionManager = new CloudServiceVersionManager();
+        this.serviceTemplateManager = new CloudServiceTemplateManager();
+
+        init();
+        initListeners();
+    }
+
+    private void initListeners(){
+
+    }
+
+    private void init(){
+        initRedis();
+        initThisService();
+        startUpdater();
+    }
+
+    private void initThisService(){
+        this.service = this.serviceManager.getService(UUID.fromString(System.getenv("serviceId"))).getImpl(CloudService.class);
+        this.service.setServiceState(ServiceState.RUNNING_UNDEFINED);
+        this.service.update();
+    }
+
+    private void initRedis() {
+        RedisCredentials redisCredentials;
+        try {
+            System.out.println(Files.REDIS_CONFIG.getFile().getAbsolutePath());
+            redisCredentials = FileWriter.readObject(Files.REDIS_CONFIG.getFile(), RedisCredentials.class);
+        } catch (Exception e) {
+            this.console.error("Failed to read redis.json file! Please check your credentials.");
+            return;
+        }
+        this.redisConnection = new RedisConnection(redisCredentials);
+        try {
+            this.redisConnection.connect();
+            this.console.info("Redis connection established!");
+        } catch (Exception e) {
+            this.console.error("§cFailed to connect to redis server. Please check your credentials.", e);
+        }
+    }
+
+    private void startUpdater(){
+        this.updaterTask = ProxyServer.getInstance().getScheduler().schedule(this.plugin, () -> {
+            this.service.setOnlineCount(ProxyServer.getInstance().getOnlineCount());
+            long usedRam = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+            this.service.setRamUsage(usedRam);
+            this.service.updateAsync();
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void updateApplicationProperties(CloudService object) {
+        if(!object.getUniqueId().equals(service.getUniqueId())) return;
+
+    }
+
+    @Override
+    public IScheduler getScheduler() {
+        return null;
+    }
+
+    @Override
+    public IConsole getConsole() {
+        return null;
+    }
+
+    @Override
+    public ICommandManager<?> getCommandManager() {
+        return null;
+    }
+
+    @Override
+    public ICloudPlayerManager getPlayerManager() {
+        return null;
+    }
+
+    @Override
+    public INetworkComponentInfo getNetworkComponentInfo() {
+        return this.service.getNetworkComponentInfo();
+    }
+
+    @Override
+    public void shutdown(boolean fromHook) {
+        //TODO use cloud player
+        for (ProxiedPlayer onlinePlayer : ProxyServer.getInstance().getPlayers()) {
+            onlinePlayer.disconnect("§cServer is shutting down.");
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(this.updaterTask != null) this.updaterTask.cancel();
+
+        if(this.redisConnection != null) this.redisConnection.getClient().shutdown();
+    }
+}

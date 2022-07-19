9c8c00d7ef21b3b190cad06de9f263353cc0b9ee
Suqatri <paul@suqatri.net>
2022-07-19 23:09:46 +0200

    refactor: ByteUtils#bytesToMB

--- commons/src/main/java/net/suqatri/cloud/commons/ByteUtils.java
+++ commons/src/main/java/net/suqatri/cloud/commons/ByteUtils.java
@@ -2,8 +2,8 @@ package net.suqatri.cloud.commons;
 
 public class ByteUtils {
 
-    public static double bytesToMb(long bytes) {
-        return bytes / 1048576.0;
+    public static long bytesToMb(long bytes) {
+        return bytes / 1048576;
     }
 
 }

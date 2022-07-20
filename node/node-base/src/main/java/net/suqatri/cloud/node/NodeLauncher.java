d90c0392bd140cf7afed3f6ad9644f6e510ea25e
Suqatri <paul@suqatri.net>
2022-07-20 21:17:43 +0200

    fix: creation of static service folder

--- node/node-base/src/main/java/net/suqatri/cloud/node/NodeLauncher.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/NodeLauncher.java
@@ -402,6 +402,7 @@ public class NodeLauncher extends NodeCloudDefaultAPI {
         Files.LIBS_BLACKLIST_FOLDER.createIfNotExists();
         Files.LIBS_REPO_FOLDER.createIfNotExists();
         Files.TEMP_FOLDER.createIfNotExists();
+        Files.STATIC_SERVICE_FOLDER.createIfNotExists();
         Files.TEMP_TRANSFER_FOLDER.createIfNotExists();
         Files.TEMP_SERVICE_FOLDER.createIfNotExists();
         Files.TEMP_VERSION_FOLDER.createIfNotExists();

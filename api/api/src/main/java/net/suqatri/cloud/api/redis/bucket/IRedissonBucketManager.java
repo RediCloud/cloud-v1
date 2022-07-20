764b712cd95e3da98c447f300c3036e72c8ce067
Suqatri <paul@suqatri.net>
2022-07-20 03:36:35 +0200

    feat: add local cache methodes for RedissonBucketManager

--- api/api/src/main/java/net/suqatri/cloud/api/redis/bucket/IRedissonBucketManager.java
+++ api/api/src/main/java/net/suqatri/cloud/api/redis/bucket/IRedissonBucketManager.java
@@ -6,8 +6,17 @@ import net.suqatri.cloud.commons.function.future.FutureAction;
 public interface IRedissonBucketManager<T extends IRBucketObject> extends IRedissonManager {
 
     Class<T> getImplClass();
+
     FutureAction<IRBucketHolder<T>> getBucketHolderAsync(String identifier);
+
     IRBucketHolder<T> getBucketHolder(String identifier);
+
     void unlink(IRBucketHolder<T> bucketHolder);
+
+    boolean isBucketHolderCached(String identifier);
+
+    IRBucketHolder<T> getCachedBucketHolder(String identifier);
+
+    void removeCachedBucketHolder(String identifier);
 
 }

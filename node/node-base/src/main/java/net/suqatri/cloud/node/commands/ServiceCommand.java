4ad5f297be35ae35eaa06d285dbb06f75c8aafb4
Suqatri <paul@suqatri.net>
2022-07-21 01:07:04 +0200

    Update ServiceCommand.java

--- node/node-base/src/main/java/net/suqatri/cloud/node/commands/ServiceCommand.java
+++ node/node-base/src/main/java/net/suqatri/cloud/node/commands/ServiceCommand.java
@@ -153,7 +153,7 @@ public class ServiceCommand extends ConsoleCommand {
                                     commandSender.sendMessage("§8 »%tc Version: %hc" + versionHolder.get().getName());
                                     commandSender.sendMessage("§8 »%tc State: %hc" + serviceHolder.get().getServiceState().name());
                                     commandSender.sendMessage("§8 »%tc Players: %hc" + serviceHolder.get().getOnlineCount() + "§8/%hc" + serviceHolder.get().getMaxPlayers());
-                                    commandSender.sendMessage("§8 »%tc RAM: %hc" + serviceHolder.get().getRamUsage() + "§8/%hc" + serviceHolder.get().getMaxRam() + " MB");
+                                    commandSender.sendMessage("§8 »%tc RAM: %hc" + serviceHolder.get().getMaxRam() + " MB");
                                     StringBuilder templateBuilder = new StringBuilder();
                                     for (String templateName : serviceHolder.get().getConfiguration().getTemplateNames()) {
                                         if(!templateBuilder.toString().isEmpty()) templateBuilder.append("§8, %hc");

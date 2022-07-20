91539cdf6f62d357550b3b2ed1c8793f1e8209d8
Suqatri <paul@suqatri.net>
2022-07-20 04:00:49 +0200

    perf: use now a RMap to store connected service ids / names

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/service/CloudServiceManager.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/service/CloudServiceManager.java
@@ -1,5 +1,6 @@
 package net.suqatri.cloud.api.impl.service;
 
+import lombok.Getter;
 import net.suqatri.cloud.api.CloudAPI;
 import net.suqatri.cloud.api.impl.redis.bucket.RedissonBucketManager;
 import net.suqatri.cloud.api.impl.service.configuration.DefaultServiceStartConfiguration;
@@ -7,11 +8,13 @@ import net.suqatri.cloud.api.impl.service.packet.start.CloudFactoryServiceStartP
 import net.suqatri.cloud.api.impl.service.packet.start.CloudFactoryServiceStartResponseCloud;
 import net.suqatri.cloud.api.impl.service.packet.stop.CloudFactoryServiceStopPacket;
 import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
+import net.suqatri.cloud.api.redis.event.RedisConnectedEvent;
 import net.suqatri.cloud.api.service.ICloudService;
 import net.suqatri.cloud.api.service.ICloudServiceManager;
 import net.suqatri.cloud.api.service.configuration.IServiceStartConfiguration;
 import net.suqatri.cloud.api.service.factory.ICloudServiceFactory;
 import net.suqatri.cloud.commons.function.future.FutureAction;
+import org.redisson.api.RMap;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -19,39 +22,55 @@ import java.util.UUID;
 
 public class CloudServiceManager extends RedissonBucketManager<CloudService, ICloudService> implements ICloudServiceManager {
 
+    @Getter
+    private RMap<String, UUID> serviceIdFetcherMap;
+
     public CloudServiceManager() {
         super("service", ICloudService.class);
+        CloudAPI.getInstance().getEventManager().register(RedisConnectedEvent.class, event ->
+                this.serviceIdFetcherMap = event.getConnection().getClient().getMap("service@idFetcher"));
     }
 
     @Override
     public FutureAction<IRBucketHolder<ICloudService>> getServiceAsync(String name) {
         FutureAction<IRBucketHolder<ICloudService>> futureAction = new FutureAction<>();
 
-        getServicesAsync()
-                .onFailure(futureAction)
-                .onSuccess(services -> {
-                    Optional<IRBucketHolder<ICloudService>> optional = services
-                            .parallelStream()
-                            .filter(service -> service.get().getServiceName().equalsIgnoreCase(name))
-                            .findFirst();
-                    if(optional.isPresent()) {
-                        futureAction.complete(optional.get());
-                    } else {
-                        futureAction.completeExceptionally(new IllegalArgumentException("Service not found"));
-                    }
-                });
+        this.serviceIdFetcherMap.containsKeyAsync(name).whenComplete((contains, throwable) -> {
+            if (throwable != null) {
+                futureAction.completeExceptionally(throwable);
+                return;
+            }
+            if(!contains) {
+                futureAction.completeExceptionally(new IllegalArgumentException("Service not found"));
+                return;
+            }
+            this.serviceIdFetcherMap.getAsync(name)
+                    .whenComplete((serviceId, throwable1) -> {
+                        if (throwable1 != null) {
+                            futureAction.completeExceptionally(throwable1);
+                            return;
+                        }
+                        getServiceAsync(serviceId)
+                                .onFailure(futureAction)
+                                .onSuccess(futureAction::complete);
+            });
+        });
 
         return futureAction;
     }
 
     @Override
     public FutureAction<IRBucketHolder<ICloudService>> getServiceAsync(UUID uniqueId) {
-        return this.getBucketHolderAsync(uniqueId.toString());
+        return this.getBucketHolderAsync(uniqueId.toString()).map(holder -> {
+            this.serviceIdFetcherMap.putAsync(holder.get().getServiceName(), holder.get().getUniqueId());
+            return holder;
+        });
     }
 
     @Override
     public IRBucketHolder<ICloudService> getService(String name) {
-        return getServices().parallelStream().filter(service -> service.get().getServiceName().equalsIgnoreCase(name)).findFirst().orElse(null);
+        if(!this.serviceIdFetcherMap.containsKey(name)) return null;
+        return getService(this.serviceIdFetcherMap.get(name));
     }
 
     @Override
@@ -125,7 +144,7 @@ public class CloudServiceManager extends RedissonBucketManager<CloudService, ICl
 
     @Override
     public FutureAction<Boolean> existsServiceAsync(String name) {
-        return getServicesAsync().map(services -> services.parallelStream().anyMatch(service -> service.get().getServiceName().equalsIgnoreCase(name)));
+        return new FutureAction<>(this.serviceIdFetcherMap.containsKey(name));
     }
 
     @Override

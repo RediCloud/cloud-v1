614c90a48dad7c12a0c75a9a1c0d974d88048aee
Suqatri <paul@suqatri.net>
2022-07-21 01:07:57 +0200

    perf: update service player count on join/quit

---
+++ plugin/plugin-proxy/src/main/java/net/suqatri/cloud/plugin/proxy/listener/PostLoginListener.java
@@ -0,0 +1,17 @@
+package net.suqatri.cloud.plugin.proxy.listener;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.suqatri.cloud.plugin.proxy.ProxyCloudAPI;
+
+public class PostLoginListener implements Listener {
+
+    @EventHandler
+    public void onPostLogin(PostLoginEvent event){
+        ProxyCloudAPI.getInstance().getService().setOnlineCount(ProxyServer.getInstance().getOnlineCount());
+        ProxyCloudAPI.getInstance().getService().updateAsync();
+    }
+
+}

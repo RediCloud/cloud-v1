70091d31b8a983e60bb471f4871c44eb7c89005c
Suqatri <paul@suqatri.net>
2022-07-21 15:45:08 +0200

    Update ITimeOutPoll.java

--- api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/poll/timeout/ITimeOutPoll.java
+++ api/api-default-impl/src/main/java/net/suqatri/cloud/api/impl/poll/timeout/ITimeOutPoll.java
@@ -3,6 +3,7 @@ package net.suqatri.cloud.api.impl.poll.timeout;
 import net.suqatri.cloud.commons.function.future.FutureAction;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.UUID;
 
 public interface ITimeOutPoll extends Serializable {
@@ -11,9 +12,14 @@ public interface ITimeOutPoll extends Serializable {
     UUID getTimeOutTargetId();
     UUID getOpenerId();
 
+    HashMap<UUID, TimeOutResult> getResults();
+
     FutureAction<TimeOutResult> decide();
+
     void manageResult(TimeOutResult result, UUID nodeId);
 
-    boolean isOpenerId(UUID uniqueId);
+    void close();
+
+    boolean isOpenerId();
 
 }

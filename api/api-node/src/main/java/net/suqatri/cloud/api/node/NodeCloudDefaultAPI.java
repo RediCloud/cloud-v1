f3bf78ca7a003ec252a82f60abf0126111e79806
Suqatri <paul@suqatri.net>
2022-07-21 04:51:13 +0200

    Update NodeCloudDefaultAPI.java

--- api/api-node/src/main/java/net/suqatri/cloud/api/node/NodeCloudDefaultAPI.java
+++ api/api-node/src/main/java/net/suqatri/cloud/api/node/NodeCloudDefaultAPI.java
@@ -2,11 +2,9 @@ package net.suqatri.cloud.api.node;
 
 import net.suqatri.cloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.cloud.api.impl.node.CloudNode;
-import net.suqatri.cloud.api.impl.redis.bucket.RBucketHolder;
-import net.suqatri.cloud.api.node.ICloudNode;
+import net.suqatri.cloud.api.impl.poll.timeout.ITimeOutPollManager;
 import net.suqatri.cloud.api.node.file.IFileTransferManager;
 import net.suqatri.cloud.api.node.service.screen.IServiceScreenManager;
-import net.suqatri.cloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.cloud.api.utils.ApplicationType;
 
 public abstract class NodeCloudDefaultAPI extends CloudDefaultAPIImpl<CloudNode> {
@@ -18,5 +16,6 @@ public abstract class NodeCloudDefaultAPI extends CloudDefaultAPIImpl<CloudNode>
     public abstract CloudNode getNode();
     public abstract IFileTransferManager getFileTransferManager();
     public abstract IServiceScreenManager getScreenManager();
+    public abstract ITimeOutPollManager timeOutPoolManager;
 
 }

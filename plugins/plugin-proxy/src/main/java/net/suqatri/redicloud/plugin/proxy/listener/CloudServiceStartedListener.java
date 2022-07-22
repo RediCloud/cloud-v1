86f4e54686201867ae7b4c5d1292361ba5f83ab3
Suqatri <paul@suqatri.net>
2022-07-22 23:55:04 +0200

    add service notify listener

--- plugins/plugin-proxy/src/main/java/net/suqatri/redicloud/plugin/proxy/listener/CloudServiceStartedListener.java
+++ plugins/plugin-proxy/src/main/java/net/suqatri/redicloud/plugin/proxy/listener/CloudServiceStartedListener.java
@@ -2,29 +2,42 @@ package net.suqatri.redicloud.plugin.proxy.listener;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.suqatri.redicloud.api.CloudAPI;
 import net.suqatri.redicloud.api.event.CloudListener;
 import net.suqatri.redicloud.api.service.ServiceEnvironment;
 import net.suqatri.redicloud.api.service.event.CloudServiceStartedEvent;
+import net.suqatri.redicloud.plugin.proxy.ProxyCloudAPI;
 
 import java.net.InetSocketAddress;
 
-public class CloudServiceStartListener {
+public class CloudServiceStartedListener {
 
     @CloudListener
     public void onCloudServiceStart(CloudServiceStartedEvent event) {
         event.getServiceAsync()
                 .onFailure(e -> CloudAPI.getInstance().getConsole().error("Failed to register service!", e))
                 .onSuccess(serviceHolder -> {
-                    if(serviceHolder.get().getEnvironment() != ServiceEnvironment.MINECRAFT) return;
+                    if(serviceHolder.get().getEnvironment() == ServiceEnvironment.PROXY) return;
                     ServerInfo serverInfo = ProxyServer.getInstance().constructServerInfo(
                                     serviceHolder.get().getServiceName(),
-                                    InetSocketAddress.createUnresolved(serviceHolder.get().getHostName(), serviceHolder.get().getPort()),
+                                    new InetSocketAddress(serviceHolder.get().getHostName(), serviceHolder.get().getPort()),
                                     serviceHolder.get().getMotd(),
                                     false);
 
                     ProxyServer.getInstance().getServers().put(serverInfo.getName(), serverInfo);
                     CloudAPI.getInstance().getConsole().debug("Registered service: " + serviceHolder.get().getServiceName());
+
+                    CloudAPI.getInstance().getNodeManager().getNodeAsync(serviceHolder.get().getNodeId())
+                        .onFailure(e -> CloudAPI.getInstance().getConsole().error("Failed to get node!", e))
+                        .onSuccess(nodeHolder -> {
+
+                            for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
+                                if(!player.hasPermission("redicloud.service.notify")) continue;
+                                player.sendMessage(ProxyCloudAPI.getInstance().getChatPrefix()
+                                        + "§3" + serviceHolder.get().getServiceName() + "§8(§f" + nodeHolder.get().getName() + "§8) » §a§l■");
+                            }
+                        });
                 });
     }
 
---
+++ plugins/plugin-proxy/src/main/java/net/suqatri/redicloud/plugin/proxy/listener/CloudServiceStoppedListener.java
@@ -0,0 +1,31 @@
+package net.suqatri.redicloud.plugin.proxy.listener;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.suqatri.redicloud.api.CloudAPI;
+import net.suqatri.redicloud.api.event.CloudListener;
+import net.suqatri.redicloud.api.service.event.CloudServiceStoppedEvent;
+import net.suqatri.redicloud.plugin.proxy.ProxyCloudAPI;
+
+public class CloudServiceStoppedListener {
+
+    @CloudListener
+    public void onCloudServiceStart(CloudServiceStoppedEvent event) {
+        ServerInfo serverInfo = ProxyServer.getInstance().getServers().get(event.getServiceName());
+        if(serverInfo != null) {
+            ProxyServer.getInstance().getServers().remove(serverInfo.getName());
+            CloudAPI.getInstance().getConsole().debug("Unregistered service: " + event.getServiceName());
+        }
+        CloudAPI.getInstance().getNodeManager().getNodeAsync(event.getNodeId())
+                .onFailure(e -> CloudAPI.getInstance().getConsole().error("Failed to unregister service: " + event.getServiceName(), e))
+                .onSuccess(nodeHolder -> {
+                        for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
+                            if(!player.hasPermission("redicloud.service.notify")) continue;
+                            player.sendMessage(ProxyCloudAPI.getInstance().getChatPrefix()
+                                    + "§3" + event.getServiceName() + "§8(§f" + nodeHolder.get().getName() + "§8) » §4§l■");
+                        }
+                });
+    }
+
+}

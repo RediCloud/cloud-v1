2707fdd084d668d2e0e797567c3cc221022e552a
Suqatri <paul@suqatri.net>
2022-07-31 01:20:00 +0200

    Update RedisSetup.java

--- node/node-base/src/main/java/net/suqatri/redicloud/node/setup/redis/RedisSetup.java
+++ node/node-base/src/main/java/net/suqatri/redicloud/node/setup/redis/RedisSetup.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import net.suqatri.redicloud.node.NodeLauncher;
 import net.suqatri.redicloud.node.console.setup.Setup;
 import net.suqatri.redicloud.node.console.setup.SetupHeaderBehaviour;
+import net.suqatri.redicloud.node.console.setup.annotations.AcceptEmptyInput;
 import net.suqatri.redicloud.node.console.setup.annotations.AnswerCompleter;
 import net.suqatri.redicloud.node.console.setup.annotations.Question;
 import net.suqatri.redicloud.node.console.setup.suggester.HostNameSuggester;
@@ -20,6 +21,7 @@ public class RedisSetup extends Setup<RedisSetup> {
     @AnswerCompleter(value = RedisPortSuggester.class)
     private int port;
 
+    @AcceptEmptyInput
     @Question(id = 3, question = "What´s the password of the Redis server?")
     private String password;
 

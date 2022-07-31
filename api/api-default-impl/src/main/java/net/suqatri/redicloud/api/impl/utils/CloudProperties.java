9eec6942cf2a0ee6df358cbcfc7ef6efa4e65b6b
Suqatri <paul@suqatri.net>
2022-07-31 12:43:13 +0200

    Create CloudProperties.java

---
+++ api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/utils/CloudProperties.java
@@ -0,0 +1,35 @@
+package net.suqatri.redicloud.api.impl.utils;
+
+import lombok.Data;
+import lombok.Getter;
+import net.suqatri.redicloud.api.utils.ICloudProperties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Getter
+public class CloudProperties implements ICloudProperties {
+
+    private final Properties properties;
+
+    public CloudProperties(InputStream inputStream) throws IOException {
+        this.properties = new Properties();
+        this.properties.load(inputStream);
+    }
+
+    @Override
+    public String getVersion() {
+        return this.properties.getProperty("version", "unknown");
+    }
+
+    @Override
+    public String getGitHash() {
+        return this.properties.getProperty("git-hash", "unknown");
+    }
+
+    @Override
+    public String getGitBrancheName() {
+        return this.properties.getProperty("git-branch", "unknown");
+    }
+}

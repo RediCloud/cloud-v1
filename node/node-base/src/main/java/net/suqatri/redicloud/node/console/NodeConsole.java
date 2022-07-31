872076cb2e198c3539fae2f77c9446b29e803d1d
Suqatri <paul@suqatri.net>
2022-07-31 16:31:22 +0200

    style: git hash style

--- node/node-base/src/main/java/net/suqatri/redicloud/node/console/NodeConsole.java
+++ node/node-base/src/main/java/net/suqatri/redicloud/node/console/NodeConsole.java
@@ -72,7 +72,7 @@ public class NodeConsole implements IConsole {
         log(new ConsoleLine("", "").setPrintPrefix(false).setPrintTimestamp(false));
         log(new ConsoleLine("", this.textColor + "    A redis based cluster cloud system for Minecraft").setPrintPrefix(false).setPrintTimestamp(false));
         log(new ConsoleLine("", "    §8» " + this.textColor + "Version: " + this.highlightColor + CloudAPI.getInstance().getProperties().getVersion() + " §8| " + this.textColor + "Discord: " + this.highlightColor + "https://discord.gg/g2HV52VV4G").setPrintPrefix(false).setPrintTimestamp(false));
-        log(new ConsoleLine("", "    §8» " + this.textColor + "Git: " + CloudAPI.getInstance().getProperties().getGitHash()).setPrintPrefix(false).setPrintTimestamp(false));
+        log(new ConsoleLine("", "    §8» " + this.textColor + "Git: " + this.highlightColor + CloudAPI.getInstance().getProperties().getGitHash()).setPrintPrefix(false).setPrintTimestamp(false));
         log(new ConsoleLine("", "     ").setPrintPrefix(false).setPrintTimestamp(false));
         if (this.cleanConsoleMode && printWarning) {
             warn("§cClean console mode is enabled! Stacktraces will not be printed, only the message.");

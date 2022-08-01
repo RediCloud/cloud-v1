90c71812d143706a33932989265be353144610d4
Suqatri <paul@suqatri.net>
2022-08-01 16:56:45 +0200

    fix: proxy plugin load

--- plugins/plugin-proxy/src/main/java/net/suqatri/redicloud/plugin/proxy/ProxyCloudAPI.java
+++ plugins/plugin-proxy/src/main/java/net/suqatri/redicloud/plugin/proxy/ProxyCloudAPI.java
@@ -12,6 +12,7 @@ import net.suqatri.redicloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.redicloud.api.impl.group.CloudGroup;
 import net.suqatri.redicloud.api.impl.player.CloudPlayerManager;
 import net.suqatri.redicloud.api.impl.redis.RedisConnection;
+import net.suqatri.redicloud.api.impl.redis.bucket.RedissonBucketManager;
 import net.suqatri.redicloud.api.impl.service.CloudService;
 import net.suqatri.redicloud.api.impl.service.CloudServiceManager;
 import net.suqatri.redicloud.api.impl.service.factory.CloudServiceFactory;
@@ -25,6 +26,7 @@ import net.suqatri.redicloud.api.service.ServiceEnvironment;
 import net.suqatri.redicloud.api.service.ServiceState;
 import net.suqatri.redicloud.api.service.configuration.IServiceStartConfiguration;
 import net.suqatri.redicloud.api.service.event.CloudServiceStartedEvent;
+import net.suqatri.redicloud.api.service.event.CloudServiceStoppedEvent;
 import net.suqatri.redicloud.api.utils.ApplicationType;
 import net.suqatri.redicloud.api.utils.Files;
 import net.suqatri.redicloud.commons.file.FileWriter;
@@ -36,9 +38,9 @@ import net.suqatri.redicloud.plugin.proxy.service.CloudProxyServiceManager;
 import org.checkerframework.checker.units.qual.C;
 
 import java.io.File;
+import java.lang.management.ManagementFactory;
 import java.net.InetSocketAddress;
-import java.util.Locale;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Getter
@@ -84,7 +86,7 @@ public class ProxyCloudAPI extends CloudDefaultAPIImpl<CloudService> {
             this.shutdown(false);
             return;
         }
-        if(!hasGroupId()){
+        if(!hasServiceId() && !hasGroupId()){
             this.console.fatal("Group id is not set as environment variable!", null);
             this.shutdown(false);
             return;
@@ -145,6 +147,16 @@ public class ProxyCloudAPI extends CloudDefaultAPIImpl<CloudService> {
                 });
     }
 
+    private List<String> getStartArguments(){
+        List<String> list = new ArrayList<>();
+        for (String s : System.getProperty("sun.java.command").split(" ")) {
+            if(s.startsWith("--")){
+                list.add(s.replaceFirst("--", ""));
+            }
+        }
+        return list;
+    }
+
     private UUID getServiceId(){
         return UUID.fromString(System.getenv("redicloud_service_id"));
     }
@@ -154,41 +166,90 @@ public class ProxyCloudAPI extends CloudDefaultAPIImpl<CloudService> {
     }
 
     private String getRedisFilePath(){
-        return System.getenv("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase());
+        if(System.getenv().containsKey("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase())) {
+            return System.getenv("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase());
+        }
+        for (String inputArgument : getStartArguments()) {
+            if(inputArgument.startsWith("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase())){
+                String[] split = inputArgument.split("=");
+                if(split.length == 2){
+                    return split[1];
+                }
+            }
+        }
+        return null;
     }
 
     private boolean hasRedisFilePath(){
-        return System.getenv().containsKey("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase());
+        return System.getenv().containsKey("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase())
+                || getStartArguments().parallelStream()
+                .anyMatch(s -> s.startsWith("redicloud_files_" + Files.REDIS_CONFIG.name().toLowerCase()));
     }
 
     private boolean isRunningExternal(){
-        return System.getenv().containsKey("redicloud_external") && System.getenv("redicloud_external").equals("true");
+        return (System.getenv().containsKey("redicloud_external")
+                && System.getenv("redicloud_external").equals("true"))
+                ||
+                getStartArguments().parallelStream()
+                        .anyMatch(s -> s.startsWith("redicloud_external=true"));
     }
 
     private UUID getGroupId() {
-        return UUID.fromString(System.getenv("redicloud_group_id"));
+        if(System.getenv().containsKey("redicloud_group_id")) {
+            return UUID.fromString(System.getenv("redicloud_group_id"));
+        }
+        for (String inputArgument : getStartArguments()) {
+            if(inputArgument.startsWith("redicloud_group_id")){
+                String[] split = inputArgument.split("=");
+                if(split.length == 2){
+                    return UUID.fromString(split[1]);
+                }
+            }
+        }
+        return null;
     }
 
     private boolean hasGroupId() {
-        return System.getenv().containsKey("redicloud_group_id");
+        return System.getenv().containsKey("redicloud_group_id")
+                || getStartArguments().parallelStream()
+                .anyMatch(s -> s.startsWith("redicloud_group_id"));
     }
 
     private void initThisService() {
 
         this.service = null;
 
+        if(CloudAPI.getInstance().getNodeManager().getNodes().parallelStream().noneMatch(holder -> holder.get().isConnected())) {
+            this.console.fatal("Cluster seems to be offline! There are no connected nodes!", null);
+            this.shutdown(false);
+            return;
+        }
+
         if(!hasServiceId()){
             this.runningExternal = true;
             if(this.getGroupManager().existsGroup(getGroupId())){
+
                 IServiceStartConfiguration startConfiguration = this.getGroupManager()
                         .getGroup(this.getGroupId()).getImpl(CloudGroup.class).createServiceConfiguration();
+
+                int id = 1;
+                List<Integer> ids = new ArrayList<>();
+                for (IRBucketHolder<ICloudService> serviceHolder : this.serviceManager.getServices()) {
+                    if(serviceHolder.get().getServiceState() == ServiceState.OFFLINE && startConfiguration.isStatic()) continue;
+                    if (serviceHolder.get().getGroupName().equalsIgnoreCase(startConfiguration.getGroupName())) {
+                        ids.add(serviceHolder.get().getId());
+                    }
+                }
+                while (ids.contains(id)) id++;
+
                 this.service = new CloudService();
+                this.service.setConfiguration(startConfiguration);
+                this.service.getConfiguration().setId(id);
                 this.service.setExternal(true);
                 this.service.setServiceState(ServiceState.RUNNING_UNDEFINED);
                 this.service.setMaxPlayers(50);
                 this.service.setMotd("§7•§8● §bRedi§3Cloud §8» §fA §bredis §fbased §bcluster §fcloud system§r\n    §b§l§8× §fDiscord §8➜ §3https://discord.gg/g2HV52VV4G");
                 this.service.setNodeId(null);
-                this.service.setConfiguration(startConfiguration);
                 this.service = this.serviceManager.createBucket(this.service.getUniqueId().toString(), this.service).getImpl(CloudService.class);
             }else{
                 this.console.fatal("No target group found for external service " + getServiceId(), null);
@@ -218,7 +279,9 @@ public class ProxyCloudAPI extends CloudDefaultAPIImpl<CloudService> {
     private void initRedis() {
         RedisCredentials redisCredentials;
         try {
-            redisCredentials = FileWriter.readObject(new File(getRedisFilePath()), RedisCredentials.class);
+            String path = getRedisFilePath();
+            this.console.debug("Using redis config file: " + path);
+            redisCredentials = FileWriter.readObject(new File(path), RedisCredentials.class);
         } catch (Exception e) {
             this.console.error("Failed to read redis.json file! Please check your credentials.");
             return;
@@ -262,7 +325,20 @@ public class ProxyCloudAPI extends CloudDefaultAPIImpl<CloudService> {
         } catch (InterruptedException e) {
         }
 
+        if(this.service != null){
+            if(this.service.isExternal()){
+                CloudAPI.getInstance().getEventManager().postGlobalAsync(new CloudServiceStoppedEvent(this.service.getHolder()));
+            }
+        }
+
         if (this.updaterTask != null) this.updaterTask.cancel();
+
+        if(this.service != null) {
+            this.service.setServiceState(ServiceState.OFFLINE);
+            this.service.update();
+            ((RedissonBucketManager) CloudAPI.getInstance().getServiceManager()).deleteBucket(this.service.getUniqueId().toString());
+            CloudAPI.getInstance().getServiceManager().removeFromFetcher(this.service.getServiceName(), this.service.getUniqueId());
+        }
 
         if (this.redisConnection != null) this.redisConnection.getClient().shutdown();
 

171d8b0e1c4122926195f52c824c8405292c744d
Suqatri <paul@suqatri.net>
2022-08-02 01:15:02 +0200

    remove some trace lines

--- api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/service/CloudServiceManager.java
+++ api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/service/CloudServiceManager.java
@@ -36,19 +36,16 @@ public abstract class CloudServiceManager extends RedissonBucketManager<CloudSer
 
     @Override
     public void putInFetcher(String serviceName, UUID serviceId) {
-        CloudAPI.getInstance().getConsole().trace("Putting service " + serviceName + " in fetcher");
         this.serviceIdFetcherMap.putAsync(serviceName.toLowerCase(), serviceId.toString());
     }
 
     @Override
     public void removeFromFetcher(String serviceName) {
-        CloudAPI.getInstance().getConsole().trace("Removing service " + serviceName + " from fetcher");
         this.serviceIdFetcherMap.removeAsync(serviceName.toLowerCase());
     }
 
     @Override
     public void removeFromFetcher(String serviceName, UUID serviceId) {
-        CloudAPI.getInstance().getConsole().trace("Removing service " + serviceName + "|" + serviceId + " from fetcher");
         this.serviceIdFetcherMap.removeAsync(serviceName.toLowerCase(), serviceId.toString());
     }
 

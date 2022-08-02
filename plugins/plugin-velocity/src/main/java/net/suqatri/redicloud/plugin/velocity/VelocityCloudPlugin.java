552f7325ce812629765e36910930a005b1b2f555
Suqatri <paul@suqatri.net>
2022-08-02 17:03:39 +0200

    create api classes

---
+++ plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
@@ -0,0 +1,17 @@
+package net.suqatri.redicloud.plugin.velocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+@Plugin(id = "redicloud-plugin", name = "RediCloud-Plugin", authors = "Suqatri")
+public class VelocityCloudPlugin {
+
+    private VelocityCloudAPI cloudAPI;
+
+    @Inject
+    public VelocityCloudPlugin(ProxyServer proxyServer) {
+        cloudAPI = new VelocityCloudAPI(proxyServer, this);
+    }
+
+}

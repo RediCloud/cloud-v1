f802c1647bc0f3436ef03dd6da82f6ff38fe5dbb
Suqatri <paul@suqatri.net>
2022-08-02 18:57:52 +0200

    fix: velocity plugin start

--- plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
+++ plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
@@ -4,13 +4,15 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 
+import java.util.logging.Logger;
+
 @Plugin(id = "redicloud-plugin", name = "RediCloud-Plugin", version = "1.0-SNAPSHOT", authors = "Suqatri")
 public class VelocityCloudPlugin {
 
     private VelocityCloudAPI cloudAPI;
 
     @Inject
-    public VelocityCloudPlugin(ProxyServer proxyServer) {
+    public VelocityCloudPlugin(ProxyServer proxyServer, Logger logger) {
         cloudAPI = new VelocityCloudAPI(proxyServer, this);
     }
 

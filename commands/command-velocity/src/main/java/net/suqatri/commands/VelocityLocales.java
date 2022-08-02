e5a849b695d6c7c9dbdf22460446c1b2ed1b6382
Suqatri <paul@suqatri.net>
2022-08-02 17:01:58 +0200

    add velocity commands

---
+++ commands/command-velocity/src/main/java/net/suqatri/commands/VelocityLocales.java
@@ -0,0 +1,21 @@
+package net.suqatri.commands;
+
+import java.util.Locale;
+
+public class VelocityLocales extends Locales {
+    private final VelocityCommandManager manager;
+
+    public VelocityLocales(VelocityCommandManager manager) {
+        super(manager);
+
+        this.manager = manager;
+        this.addBundleClassLoader(this.manager.getPlugin().getClass().getClassLoader());
+    }
+
+    @Override
+    public void loadLanguages() {
+        super.loadLanguages();
+        String pluginName = "acf-" + manager.plugin.getDescription().getName().get();
+        addMessageBundles("acf-minecraft", pluginName, pluginName.toLowerCase(Locale.ENGLISH));
+    }
+}

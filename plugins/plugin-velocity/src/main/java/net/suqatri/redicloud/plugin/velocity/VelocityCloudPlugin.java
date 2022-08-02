64e1dfd9869b2d988c8e68fceb2e66e3c968f6e9
Suqatri <paul@suqatri.net>
2022-08-02 18:54:33 +0200

    Update VelocityCloudPlugin.java

--- plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
+++ plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
@@ -4,7 +4,7 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 
-@Plugin(id = "redicloud-plugin", name = "RediCloud-Plugin", authors = "Suqatri")
+@Plugin(id = "redicloud-plugin", name = "RediCloud-Plugin", version = "1.0-SNAPSHOT", authors = "Suqatri")
 public class VelocityCloudPlugin {
 
     private VelocityCloudAPI cloudAPI;

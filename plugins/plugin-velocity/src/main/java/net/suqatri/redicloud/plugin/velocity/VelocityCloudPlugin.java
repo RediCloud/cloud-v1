f6354dbfe951401fd25f8afe4ee519d71c144d77
Suqatri <paul@suqatri.net>
2022-08-02 19:24:08 +0200

    Update VelocityCloudPlugin.java

--- plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
+++ plugins/plugin-velocity/src/main/java/net/suqatri/redicloud/plugin/velocity/VelocityCloudPlugin.java
@@ -5,6 +5,8 @@ import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
+import java.util.concurrent.TimeUnit;
+
 @Plugin(id = "redicloud-plugin", name = "RediCloud-Plugin", version = "1.0-SNAPSHOT", authors = "Suqatri")
 public class VelocityCloudPlugin {
 
@@ -12,7 +14,10 @@ public class VelocityCloudPlugin {
 
     @Inject
     public VelocityCloudPlugin(ProxyServer proxyServer, Logger logger) {
-        cloudAPI = new VelocityCloudAPI(proxyServer, this);
+        //TODO fix this
+        proxyServer.getScheduler().buildTask(this, () -> {
+            cloudAPI = new VelocityCloudAPI(proxyServer, this);
+        }).delay(2, TimeUnit.SECONDS).schedule();
     }
 
 }

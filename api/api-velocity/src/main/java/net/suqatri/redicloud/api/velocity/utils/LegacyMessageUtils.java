877a6fc2f7552367ef081cb1fadb4dd5b3fba5c9
Suqatri <paul@suqatri.net>
2022-08-03 05:50:21 +0200

    move LegacyMessageUtils to default velocity api module

--- api/api-velocity/src/main/java/net/suqatri/redicloud/api/velocity/utils/LegacyMessageUtils.java
+++ api/api-velocity/src/main/java/net/suqatri/redicloud/api/velocity/utils/LegacyMessageUtils.java
@@ -1,4 +1,4 @@
-package net.suqatri.redicloud.plugin.velocity.utils;
+package net.suqatri.redicloud.api.velocity.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;

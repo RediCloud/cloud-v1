7a7b48b163abd9651204990e16923097256fdc49
Suqatri <paul@suqatri.net>
2022-08-03 05:46:31 +0200

    create and register bridge packets

---
+++ api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/player/packet/CloudBridgeTabPacket.java
@@ -0,0 +1,24 @@
+package net.suqatri.redicloud.api.impl.player.packet;
+
+import lombok.Data;
+import net.suqatri.redicloud.api.CloudAPI;
+import net.suqatri.redicloud.api.impl.packet.CloudPacket;
+
+import java.util.UUID;
+
+@Data
+public class CloudBridgeTabPacket extends CloudPacket {
+
+    private UUID uniqueId;
+    private String header;
+    private String footer;
+
+    @Override
+    public void receive() {
+        CloudAPI.getInstance().getPlayerManager().getPlayerAsync(this.uniqueId)
+            .onFailure(t -> CloudAPI.getInstance().getConsole().error("Failed to send title to player " + this.uniqueId, t))
+            .onSuccess(playerHolder -> {
+                playerHolder.get().getBridge().sendTab(this.header, this.footer);
+            });
+    }
+}

270959967f49721430a63b0525c1c78960266784
Suqatri <paul@suqatri.net>
2022-08-03 05:46:53 +0200

    moved player methodes to bridge

--- api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/player/CloudPlayer.java
+++ api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/player/CloudPlayer.java
@@ -3,8 +3,10 @@ package net.suqatri.redicloud.api.impl.player;
 import lombok.Getter;
 import lombok.Setter;
 import net.suqatri.redicloud.api.CloudAPI;
+import net.suqatri.redicloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.redicloud.api.impl.redis.bucket.RBucketObject;
 import net.suqatri.redicloud.api.player.ICloudPlayer;
+import net.suqatri.redicloud.api.player.IPlayerBridge;
 import net.suqatri.redicloud.api.redis.bucket.IRBucketHolder;
 import net.suqatri.redicloud.api.service.ICloudService;
 import net.suqatri.redicloud.commons.function.future.FutureAction;
@@ -25,12 +27,13 @@ public class CloudPlayer extends RBucketObject implements ICloudPlayer {
     private UUID lastConnectedServerId;
     private UUID lastConnectedProxyId;
     private boolean connected;
+    private IPlayerBridge bridge = CloudDefaultAPIImpl.getInstance().createBridge(this.getHolder());
 
     @Override
     public FutureAction<IRBucketHolder<ICloudService>> getServer() {
         FutureAction<IRBucketHolder<ICloudService>> futureAction = new FutureAction<>();
         if (!isConnected()) {
-            futureAction.completeExceptionally(new IllegalStateException("player@" + this.getUniqueId() + " is not connected"));
+            futureAction.completeExceptionally(new IllegalStateException("player:" + this.getUniqueId() + " is not connected"));
             return futureAction;
         }
         CloudAPI.getInstance().getServiceManager().getServiceAsync(this.getLastConnectedServerId())
@@ -43,48 +46,13 @@ public class CloudPlayer extends RBucketObject implements ICloudPlayer {
     public FutureAction<IRBucketHolder<ICloudService>> getProxy() {
         FutureAction<IRBucketHolder<ICloudService>> futureAction = new FutureAction<>();
         if (!isConnected()) {
-            futureAction.completeExceptionally(new IllegalStateException("player@" + this.getUniqueId() + " is not connected"));
+            futureAction.completeExceptionally(new IllegalStateException("player:" + this.getUniqueId() + " is not connected"));
             return futureAction;
         }
         CloudAPI.getInstance().getServiceManager().getServiceAsync(this.getLastConnectedProxyId())
                 .onFailure(futureAction)
                 .onSuccess(futureAction::complete);
         return futureAction;
-    }
-
-    @Override
-    public void sendMessage(String message) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public void sendTitle(String title, String subTitle, int fadeIn, int stay, int fadeOut) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public void sendActionbar(String message) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public void sendTab(String header, String footer) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public void connect(String server) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public void connect(IRBucketHolder<ICloudService> cloudService) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public void disconnect(String reason) {
-        throw new UnsupportedOperationException("Not implemented yet");
     }
 
 }

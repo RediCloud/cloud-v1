bfd83cd717c1aa9a1f472b03d7a4ccc6f932b0c2
Suqatri <paul@suqatri.net>
2022-08-04 21:49:13 +0200

    add cluster support for redis credentials

--- api/api/src/main/java/net/suqatri/redicloud/api/redis/RedisCredentials.java
+++ api/api/src/main/java/net/suqatri/redicloud/api/redis/RedisCredentials.java
@@ -4,14 +4,34 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 @Getter
 @Setter
 public class RedisCredentials implements Serializable {
 
-    private String hostname;
-    private int port;
+    private HashMap<String, Integer> nodeAddresses;
     private String password;
     private int databaseId;
+    private RedisType type;
+    private boolean ssl = false;
+
+    public String toNodeAddress(String hostname){
+        return this.ssl
+                ? "rediss://" + hostname + ":" + this.nodeAddresses.get(hostname)
+                : "redis://" + hostname + ":" + this.nodeAddresses.get(hostname);
+    }
+
+    public String getAnyHostname(){
+        return this.toNodeAddress(this.nodeAddresses.keySet().iterator().next());
+    }
+
+    public Integer getAnyPort(String hostname){
+        return this.nodeAddresses.get(hostname);
+    }
+
+    public String getAnyNodeAddress(){
+        return this.toNodeAddress(this.getAnyHostname());
+    }
 
 }

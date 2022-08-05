3008c5817b55d7947d9a8a3f25da4791f06d587c
Suqatri <paul@suqatri.net>
2022-08-05 23:58:01 +0200

    use BaseCommand for command help

--- node/node-base/src/main/java/net/suqatri/redicloud/node/commands/CloudHelpCommand.java
+++ node/node-base/src/main/java/net/suqatri/redicloud/node/commands/CloudHelpCommand.java
@@ -1,11 +1,16 @@
 package net.suqatri.redicloud.node.commands;
 
+import net.suqatri.commands.BaseCommand;
 import net.suqatri.commands.CommandSender;
 import net.suqatri.commands.ConsoleCommand;
 import net.suqatri.commands.RootCommand;
 import net.suqatri.commands.annotation.CommandAlias;
 import net.suqatri.commands.annotation.Default;
+import net.suqatri.redicloud.api.CloudAPI;
 import net.suqatri.redicloud.node.NodeLauncher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CommandAlias("help")
 public class CloudHelpCommand extends ConsoleCommand {
@@ -13,8 +18,16 @@ public class CloudHelpCommand extends ConsoleCommand {
     @Default
     public void onHelp(CommandSender commandSender) {
         commandSender.sendMessage("§8<------------|§7 %tcGeneral help §8|------------§8>");
+        List<BaseCommand> commands = new ArrayList<>();
         for (RootCommand registeredRootCommand : NodeLauncher.getInstance().getCommandManager().getRegisteredRootCommands()) {
-            commandSender.sendMessage("%hc" + registeredRootCommand.getCommandName() + " %tchelp §8| %tcHelp for the " + registeredRootCommand.getCommandName() + " command");
+            commands.add(registeredRootCommand.getDefCommand());
+        }
+        if(commands.isEmpty()){
+            commandSender.sendMessage("No commands registered");
+            return;
+        }
+        for (BaseCommand command : commands) {
+            commandSender.sendMessage("%hc" + command.getName() + " %tchelp §8| %tcHelp for the " + command.getName() + " command");
         }
     }
 

38cc81d3290de677c2e0b668547fb80479b37713
Suqatri <paul@suqatri.net>
2022-08-05 23:49:46 +0200

    add dynamic help command

--- node/node-base/src/main/java/net/suqatri/redicloud/node/commands/CloudHelpCommand.java
+++ node/node-base/src/main/java/net/suqatri/redicloud/node/commands/CloudHelpCommand.java
@@ -2,8 +2,10 @@ package net.suqatri.redicloud.node.commands;
 
 import net.suqatri.commands.CommandSender;
 import net.suqatri.commands.ConsoleCommand;
+import net.suqatri.commands.RootCommand;
 import net.suqatri.commands.annotation.CommandAlias;
 import net.suqatri.commands.annotation.Default;
+import net.suqatri.redicloud.node.NodeLauncher;
 
 @CommandAlias("help")
 public class CloudHelpCommand extends ConsoleCommand {
@@ -11,15 +13,9 @@ public class CloudHelpCommand extends ConsoleCommand {
     @Default
     public void onHelp(CommandSender commandSender) {
         commandSender.sendMessage("§8<------------|§7 %tcGeneral help §8|------------§8>");
-        commandSender.sendMessage("%hccluster %tchelp §8| %tcHelp for the cluster command");
-        commandSender.sendMessage("%hctemplate %tchelp §8| %tcHelp for the templates command");
-        commandSender.sendMessage("%hcgroups %tchelp §8| %tcHelp for the groups command");
-        commandSender.sendMessage("%hcstop §8| %tcStop the node");
-        commandSender.sendMessage("%hcclear §8| %tcClear the console");
-        commandSender.sendMessage("%hcserviceversions %tchelp §8| %tcHelp for the service versions command");
-        commandSender.sendMessage("%hcservice %tchelp §8| %tcHelp for the service command");
-        commandSender.sendMessage("%hcscreen %tchelp §8| %tcHelp for the screen command");
-
+        for (RootCommand registeredRootCommand : NodeLauncher.getInstance().getCommandManager().getRegisteredRootCommands()) {
+            commandSender.sendMessage("%hc" + registeredRootCommand.getCommandName() + " %tchelp §8| %tcHelp for the " + registeredRootCommand.getCommandName() + " command");
+        }
     }
 
 }

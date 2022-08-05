873968d1840124b1712d47234df843dc1a63434d
Suqatri <paul@suqatri.net>
2022-08-05 15:42:04 +0200

    fix: private class error while queue reading

--- node/node-base/src/main/java/net/suqatri/redicloud/node/service/factory/CloudNodeServiceThread.java
+++ node/node-base/src/main/java/net/suqatri/redicloud/node/service/factory/CloudNodeServiceThread.java
@@ -311,16 +311,4 @@ public class CloudNodeServiceThread extends Thread {
         return i;
     }
 
-    private static final class CloudServiceStartComparator implements Comparator<IServiceStartConfiguration> {
-
-        @Override
-        public int compare(IServiceStartConfiguration o1, IServiceStartConfiguration o2) {
-            return o1.getStartPriority() - o2.getStartPriority();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return false;
-        }
-    }
 }

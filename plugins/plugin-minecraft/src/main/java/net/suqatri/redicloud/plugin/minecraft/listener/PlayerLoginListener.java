e51b5a4a41026e51536cb09132de0559f3701837
Suqatri <paul@suqatri.net>
2022-08-05 19:01:12 +0200

    Update PlayerLoginListener.java

--- plugins/plugin-minecraft/src/main/java/net/suqatri/redicloud/plugin/minecraft/listener/PlayerLoginListener.java
+++ plugins/plugin-minecraft/src/main/java/net/suqatri/redicloud/plugin/minecraft/listener/PlayerLoginListener.java
@@ -12,7 +12,7 @@ public class PlayerLoginListener implements Listener {
 
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
-        if(!event.getPlayer().hasPermission("redicloud.maintenance.bypass") && MinecraftCloudAPI.getInstance().getService().isMaintenance()) {
+        if(!event.getPlayer().hasPermission("redicloud.maintenance.bypass") && MinecraftCloudAPI.getInstance().getService().isInMaintenance()) {
             event.setResult(PlayerLoginEvent.Result.KICK_WHITELIST);
             event.setKickMessage("This service is currently under maintenance. Please try again later.");
         }

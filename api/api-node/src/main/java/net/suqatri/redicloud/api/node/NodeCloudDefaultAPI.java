20f2e71605eaf6b8c0e4448ea2cdf4c94014e644
Suqatri <paul@suqatri.net>
2022-08-08 22:55:34 +0200

    add instance for default node api

--- api/api-node/src/main/java/net/suqatri/redicloud/api/node/NodeCloudDefaultAPI.java
+++ api/api-node/src/main/java/net/suqatri/redicloud/api/node/NodeCloudDefaultAPI.java
@@ -1,16 +1,21 @@
 package net.suqatri.redicloud.api.node;
 
+import lombok.Getter;
 import net.suqatri.redicloud.api.impl.CloudDefaultAPIImpl;
 import net.suqatri.redicloud.api.impl.node.CloudNode;
-import net.suqatri.redicloud.api.impl.poll.timeout.ITimeOutPollManager;
+import net.suqatri.redicloud.api.node.poll.timeout.ITimeOutPollManager;
 import net.suqatri.redicloud.api.node.file.IFileTransferManager;
 import net.suqatri.redicloud.api.node.service.screen.IServiceScreenManager;
 import net.suqatri.redicloud.api.utils.ApplicationType;
 
 public abstract class NodeCloudDefaultAPI extends CloudDefaultAPIImpl<CloudNode> {
 
+    @Getter
+    private static NodeCloudDefaultAPI instance;
+
     public NodeCloudDefaultAPI() {
         super(ApplicationType.NODE);
+        instance = this;
     }
 
     public abstract CloudNode getNode();

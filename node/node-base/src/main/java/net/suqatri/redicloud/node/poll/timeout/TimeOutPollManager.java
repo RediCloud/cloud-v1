7035363444b4ea371c69cbfc6d47985dbe7315a0
Suqatri <paul@suqatri.net>
2022-08-08 22:55:56 +0200

    impl reload to timeout poll

--- node/node-base/src/main/java/net/suqatri/redicloud/node/poll/timeout/TimeOutPollManager.java
+++ node/node-base/src/main/java/net/suqatri/redicloud/node/poll/timeout/TimeOutPollManager.java
@@ -2,10 +2,10 @@ package net.suqatri.redicloud.node.poll.timeout;
 
 import lombok.Getter;
 import net.suqatri.redicloud.api.CloudAPI;
-import net.suqatri.redicloud.api.impl.poll.timeout.ITimeOutPoll;
-import net.suqatri.redicloud.api.impl.poll.timeout.ITimeOutPollManager;
-import net.suqatri.redicloud.api.impl.poll.timeout.TimeOutPoolConfiguration;
-import net.suqatri.redicloud.api.impl.poll.timeout.TimeOutResult;
+import net.suqatri.redicloud.api.node.poll.timeout.ITimeOutPoll;
+import net.suqatri.redicloud.api.node.poll.timeout.ITimeOutPollManager;
+import net.suqatri.redicloud.api.node.poll.timeout.TimeOutPoolConfiguration;
+import net.suqatri.redicloud.api.node.poll.timeout.TimeOutResult;
 import net.suqatri.redicloud.api.impl.redis.bucket.RedissonBucketManager;
 import net.suqatri.redicloud.api.network.NetworkComponentType;
 import net.suqatri.redicloud.api.node.ICloudNode;
@@ -35,7 +35,7 @@ public class TimeOutPollManager extends RedissonBucketManager<TimeOutPoll, ITime
                     ? CloudAPI.getInstance().getConfigurationManager()
                         .getConfiguration(this.configuration.getIdentifier(), TimeOutPoolConfiguration.class)
                     : CloudAPI.getInstance().getConfigurationManager().create(this.configuration);
-            this.checker();
+            if(this.configuration.isEnabled()) this.checker();
         });
     }
 
@@ -106,5 +106,15 @@ public class TimeOutPollManager extends RedissonBucketManager<TimeOutPoll, ITime
     @Override
     public FutureAction<Boolean> closePoll(ITimeOutPoll poolHolder) {
         return this.deleteBucketAsync(poolHolder.getIdentifier());
+    }
+
+    @Override
+    public void configure(TimeOutPoolConfiguration configuration) {
+        if(this.task == null && configuration.isEnabled()){
+            this.checker();
+        }else if(this.task != null && !configuration.isEnabled()){
+            this.task.cancel();
+        }
+        this.configuration = configuration;
     }
 }

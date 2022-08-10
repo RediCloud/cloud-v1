975cf868827154ac4880cbda44938ba7e3f51727
Suqatri <paul@suqatri.net>
2022-08-10 08:18:38 +0200

    fix: verify / fallback service start when disabled

--- api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/configuration/impl/PlayerConfiguration.java
+++ api/api-default-impl/src/main/java/net/suqatri/redicloud/api/impl/configuration/impl/PlayerConfiguration.java
@@ -1,6 +1,7 @@
 package net.suqatri.redicloud.api.impl.configuration.impl;
 
 import lombok.Data;
+import net.suqatri.redicloud.api.CloudAPI;
 import net.suqatri.redicloud.api.impl.configuration.Configuration;
 
 import java.util.concurrent.TimeUnit;
@@ -14,4 +15,22 @@ public class PlayerConfiguration extends Configuration {
     private int maxPasswordLength = 18;
     private boolean passwordCanContainsPlayerName = false;
 
+    @Override
+    public void merged() {
+        CloudAPI.getInstance().getGroupManager().existsGroupAsync("Verify")
+                .onFailure(throwable -> CloudAPI.getInstance().getConsole().error("Error while getting verify group"))
+                .onSuccess(exists -> {
+                    if(!exists) return;
+                    CloudAPI.getInstance().getGroupManager().getGroupAsync("Verify")
+                            .onFailure(throwable -> CloudAPI.getInstance().getConsole().error("Error while getting verify group"))
+                            .onSuccess(group -> {
+                                if(this.allowCracked){
+                                    group.setMinServices(1);
+                                }else{
+                                    group.setMinServices(0);
+                                }
+                                group.updateAsync();
+                            });
+                });
+    }
 }

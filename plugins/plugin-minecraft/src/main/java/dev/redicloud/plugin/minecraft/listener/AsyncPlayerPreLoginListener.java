fa09acd65ffd8a37d0d94fb381681c87d476f2d4
Suqatri <paul@suqatri.net>
2022-08-12 18:21:01 +0200

    Update AsyncPlayerPreLoginListener.java

--- plugins/plugin-minecraft/src/main/java/dev/redicloud/plugin/minecraft/listener/AsyncPlayerPreLoginListener.java
+++ plugins/plugin-minecraft/src/main/java/dev/redicloud/plugin/minecraft/listener/AsyncPlayerPreLoginListener.java
@@ -13,24 +13,22 @@ public class AsyncPlayerPreLoginListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPreLogin(AsyncPlayerPreLoginEvent event){
-        ICloudPlayer player;
         try {
-            player = CloudAPI.getInstance().getPlayerManager().getPlayer(event.getUniqueId());
+            ICloudPlayer player = CloudAPI.getInstance().getPlayerManager().getPlayer(event.getUniqueId());
             if(player == null){
                 event.setKickMessage("Player not found");
+                event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
+                return;
+            }
+            ICloudService cloudService = CloudAPI.getInstance().getServiceManager().getService(player.getLastConnectedProxyId());
+            CloudAPI.getInstance().getConsole().debug("Player " + player.getName() + " is connecting to via " + event.getAddress().getHostAddress() + " from proxy " + cloudService.getHostName());
+            if(!event.getAddress().getHostAddress().equals(cloudService.getHostName())){
+                event.setKickMessage("You are not allowed to connect to this service!");
                 event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
             }
         }catch (Exception e){
             event.setKickMessage("Player not found");
             event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
-            return;
-        }
-        ICloudService cloudService = CloudAPI.getInstance().getServiceManager().getService(player.getLastConnectedProxyId());
-        CloudAPI.getInstance().getConsole().debug("Player " + player.getName() + " is connecting to via " + event.getAddress().getHostAddress() + " from proxy " + cloudService.getHostName());
-        if(!event.getAddress().getHostAddress().equals(cloudService.getHostName())){
-            event.setKickMessage("You are not allowed to connect to this service!");
-            event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
-            return;
         }
     }
 

47b4c5bd07d5f7c87dba968e3523b6d999b5a6fe
Suqatri <paul@suqatri.net>
2022-08-20 16:41:59 +0200

    Create RBucketFetchAble object

---
+++ api/api-default-impl/src/main/java/dev/redicloud/api/impl/redis/bucket/fetch/RBucketFetchAble.java
@@ -0,0 +1,7 @@
+package dev.redicloud.api.impl.redis.bucket.fetch;
+
+import dev.redicloud.api.impl.redis.bucket.RBucketObject;
+import dev.redicloud.api.redis.bucket.fetch.IRBucketFetchAble;
+
+public abstract class RBucketFetchAble extends RBucketObject implements IRBucketFetchAble {
+}

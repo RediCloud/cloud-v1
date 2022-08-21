80ee28cb7799dfa3e0a7c3aca53cf993e4e22c5d
Suqatri <paul@suqatri.net>
2022-08-21 13:11:16 +0200

    Create ModuleDescription.java

---
+++ module-handler/src/main/java/dev/redicloud/module/ModuleDescription.java
@@ -0,0 +1,40 @@
+package dev.redicloud.module;
+
+import dev.redicloud.api.CloudAPI;
+import dev.redicloud.api.utils.ApplicationType;
+import lombok.Data;
+import lombok.Getter;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+@Getter
+public class ModuleDescription implements Serializable {
+
+    private final String name = null;
+    private final String version = null;
+    private final HashMap<ApplicationType, String> mainClasses = null;
+    private final File file = null;
+
+    private final List<String> dependModules = null;
+    private final List<String> softDependModules = null;
+
+    public ModuleDescription(){}
+    private ModuleDescription(String name){
+
+    }
+
+    public boolean canLoad(){
+        for (ApplicationType applicationType : mainClasses.keySet())
+            if(applicationType == CloudAPI.getInstance().getApplicationType()) return true;
+
+        return false;
+    }
+
+    public static ModuleDescription ofName(String name){
+        return new ModuleDescription(name);
+    }
+
+}

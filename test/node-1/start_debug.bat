ceb10be132283facc07282cdc17a8e9120d85312
Suqatri <paul@suqatri.net>
2022-08-21 13:25:32 +0200

    update start up flags for java 9+

--- test/node-1/start_debug.bat
+++ test/node-1/start_debug.bat
@@ -1,1 +1,1 @@
-java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -jar redicloud-node-base.jar --host=127.0.0.1 --test
+java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -jar redicloud-node-base.jar --host=127.0.0.1 --test

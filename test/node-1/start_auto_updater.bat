72625f6911b1358115aec8cedb78694bb50c700c
Suqatri <paul@suqatri.net>
2022-08-22 19:48:03 +0200

    update start scripts

--- test/node-1/start_auto_updater.bat
+++ test/node-1/start_auto_updater.bat
@@ -1,2 +1,2 @@
 java -jar redicloud-auto-updater.jar
-java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -jar redicloud-runner.jar --host=127.0.0.1 --test
+java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -jar redicloud-runner.jar --host=127.0.0.1 --test --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED

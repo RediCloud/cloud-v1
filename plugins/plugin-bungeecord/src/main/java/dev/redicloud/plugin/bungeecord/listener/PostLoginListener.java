e92321d49042267b5216c9c2188f9a13889bcbd1
Suqatri <paul@suqatri.net>
2022-08-23 23:18:07 +0200

    fix: bungee online count

--- plugins/plugin-bungeecord/src/main/java/dev/redicloud/plugin/bungeecord/listener/PostLoginListener.java
+++ plugins/plugin-bungeecord/src/main/java/dev/redicloud/plugin/bungeecord/listener/PostLoginListener.java
@@ -16,7 +16,6 @@ public class PostLoginListener implements Listener {
             return;
         }
 
-        if(event.getPlayer().getPendingConnection().isOnlineMode()) return;
 
         boolean isLoggedIn = false;
 
@@ -32,6 +31,8 @@ public class PostLoginListener implements Listener {
                 BungeeCordCloudAPI.getInstance().getPlayerManager().getConnectedList().addAsync(cloudPlayer.getUniqueId().toString());
             }
         }
+
+        if(event.getPlayer().getPendingConnection().isOnlineMode()) return;
 
         if(isLoggedIn){
             event.getPlayer().sendMessage("You are logged in as " + event.getPlayer().getName() + "!");
